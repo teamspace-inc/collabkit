7121aee08e14dd1d850e574846f4d1a41574c1ff
Ville Immonen <ville.immonen@iki.fi>
2022-05-31 12:17:10 +0300

    Simplify example app

--- examples/comments/src/App.tsx
+++ examples/comments/src/App.tsx
@@ -1,31 +1,9 @@
-import { CollabKitPlayground, useMatrix } from '@collabkit/react';
+import { CollabKitPlayground } from '@collabkit/react';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const rooms = useMatrix() as any;
-
-  if (!rooms) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Public rooms:</p>
-        <ul>
-          {rooms.chunk.slice(0, 10).map((room: any) => (
-            <li key={room.room_id}>{room.name}</li>
-          ))}
-          <li>...and {rooms.total_room_count_estimate - 10} more.</li>
-        </ul>
-        <h1>components</h1>
-        <CollabKitPlayground />
-      </header>
-    </div>
-  );
+  return <CollabKitPlayground />;
 }
 
 export default App;
