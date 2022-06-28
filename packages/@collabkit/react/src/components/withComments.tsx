bb992a432310508534a97471497e406baabe408b
Namit Chadha <namit.chadha@gmail.com>
2022-06-28 21:17:36 +0100

    comments for withComments, ha

--- packages/@collabkit/react/src/components/withComments.tsx
+++ packages/@collabkit/react/src/components/withComments.tsx
@@ -5,6 +5,11 @@ import { styled } from './UIKit';
 import { WorkspaceIDContext } from './Workspace';
 import { WorkspaceContext } from './WorkspaceLoader';
 
+// Evan from LiveFlow suggested a HOC as a way to
+// create 'Commentable' things, this is a quick sketch
+// for how that might work.
+// using `withComments` wraps a component in a `Commentable`
+
 const StyledWithComments = styled('div', {
   variants: {
     hasComments: {
