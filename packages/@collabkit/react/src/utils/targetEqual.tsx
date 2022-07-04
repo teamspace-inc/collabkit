2dc5379d55086cd073d495ed2a704b2dc24b9ffd
Ville Immonen <ville.immonen@iki.fi>
2022-07-04 14:13:25 +0300

    Fix TS error

--- packages/@collabkit/react/src/utils/targetEqual.tsx
+++ packages/@collabkit/react/src/utils/targetEqual.tsx
@@ -4,11 +4,11 @@ export function targetEqual(a: Target | null, b: Target | null) {
   return (
     a !== null &&
     b !== null &&
+    a.type === 'comment' &&
+    b.type === 'comment' &&
     a.threadId === b.threadId &&
     a.type === b.type &&
     a.workspaceId === b.workspaceId &&
-    a.type === 'comment' &&
-    b.type === 'comment' &&
     a.eventId === b.eventId
   );
 }
