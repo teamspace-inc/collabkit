bc43adcc968de3ac29dac8101aa4c0411677e784
Ville Immonen <ville.immonen@iki.fi>
2022-07-08 18:36:26 +0300

    Add the demo site

--- examples/demo/vite.config.ts
+++ examples/demo/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      // with options
+      '/api': {
+        target: 'https://us-central1-collabkit-dev.cloudfunctions.net',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
