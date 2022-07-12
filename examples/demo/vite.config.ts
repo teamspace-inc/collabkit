8939955b5c108ef5042c30e0e12906be20137549
Ville Immonen <ville.immonen@iki.fi>
2022-07-12 19:03:09 +0300

    Fix Vite config for HMR of @collabkit/react

--- examples/demo/vite.config.ts
+++ examples/demo/vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@collabkit/react': resolve(__dirname, '../../packages/@collabkit/react'),
+    },
+  },
   server: {
     proxy: {
       // with options
