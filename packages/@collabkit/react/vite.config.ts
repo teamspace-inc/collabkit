b8e2a11d645f32d0919d4719eae0a6b05fbe50d2
Ville Immonen <ville.immonen@iki.fi>
2022-07-15 16:45:32 +0300

    Add Vite library mode

--- packages/@collabkit/react/vite.config.ts
+++ packages/@collabkit/react/vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), visualizer()],
+
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'CollabKit',
+      fileName: 'collabkit',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+
+  server: {
+    port: 3003,
+  },
+});
