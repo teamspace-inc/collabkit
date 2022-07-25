ec647370614bc135d84c986adb9349fa5447ad4a
Namit Chadha <namit.chadha@gmail.com>
2022-07-25 15:45:56 +0100

    Move website to www (#27)
    * responsive
    
    * copy website to www

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,16 +1,27 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { remarkCodeHike } from '@code-hike/mdx';
+const theme = require('shiki/themes/dracula.json');
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@collabkit/react': resolve(__dirname, '../packages/@collabkit/react'),
+// mdx configured as per
+// https://github.com/brillout/vite-plugin-mdx/issues/44#issuecomment-1106448872
+export default defineConfig(async () => {
+  const mdx = await import('@mdx-js/rollup');
+  return {
+    plugins: [react(), mdx.default({ remarkPlugins: [[remarkCodeHike, { theme }]] })],
+    resolve: {
+      alias: {
+        '@collabkit/react': resolve(__dirname, '../../packages/@collabkit/react'),
+      },
     },
-  },
-  server: {
-    port: 8000,
-  },
+    optimizeDeps: {
+      include: ['react/jsx-runtime'],
+    },
+
+    server: {
+      port: 8000,
+    },
+  };
 });
