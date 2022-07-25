f468fdedd3825156abe4c13a268ddf0eee4a0500
Namit Chadha <namit.chadha@gmail.com>
2022-07-25 12:15:06 +0100

    website

--- org/website/src/Home.tsx
+++ org/website/src/Home.tsx
@@ -1,4 +1,4 @@
-import { styled, HStack, VStack } from './UIKit';
+import { styled, HStack, VStack, Grid } from './UIKit';
 import { getHighlighter, setCDN } from 'shiki';
 import Example from './Example.svg';
 import CommentAnything from './CommentAnything.svg';
@@ -64,27 +64,18 @@ const Button = styled('button', {
   gap: '10px',
   border: 'none',
   zIndex: 1,
-});
-
-const BackgroundCenter = styled('div', {
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  display: 'flex',
-  height: '100vh',
-  width: '100%',
-  overflow: 'hidden',
-  alignItems: 'center',
-  justifyContent: 'center',
+  cursor: 'pointer',
+  '&:hover': {
+    background: 'black',
+  },
 });
 
 const Section = styled('section', {
-  padding: '10vh',
-  minHeight: '80vh',
+  minHeight: '100vh',
   position: 'relative',
   display: 'flex',
+  width: '100%',
+  overflow: 'hidden',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
@@ -95,15 +86,24 @@ const Header = styled('header', {
   alignItems: 'baseline',
   justifyContent: 'stretch',
   flex: 1,
-  width: '80vw',
-  padding: '0 10vw',
+  width: '90vw',
+  padding: '0 5vw',
   position: 'fixed',
   zIndex: 2,
 });
 
+const Link = styled('a', {
+  textDecoration: 'none',
+  color: 'black',
+  '&:hover': {
+    textDecoration: 'underline',
+  },
+});
+
 const Circle = styled('div', {
-  width: '44vw',
-  height: '44vw',
+  aspectRatio: 1,
+  minWidth: '40rem',
+  maxWidth: '66vw',
   borderRadius: '50%',
   background: '#FFEC6B',
   zIndex: 0,
@@ -133,13 +133,25 @@ const Circle = styled('div', {
 });
 
 const Article = styled('article', {
-  display: 'flex',
-  gap: '180px',
+  display: 'grid',
   alignItems: 'center',
-  justifyContent: 'flex-start',
-  flexDirection: 'row',
-  marginBottom: '80px',
+  justifyContent: 'center',
+  marginBottom: '12rem',
+  gridTemplateColumns: '20rem 27.5rem',
+  columnGap: '8rem',
+
+  img: {
+    width: '100%',
+  },
 });
+
+const RequestDemoButton = (
+  <Button
+    onClick={() => (window.location.href = 'https://calendly.com/namit-chadha/30min?month=2022-07')}
+  >
+    Request a demo
+  </Button>
+);
 
 export function Home() {
   useEffect(() => {
@@ -156,46 +168,76 @@ export function Home() {
   }, []);
 
   return (
-    <div>
+    <div style={{}}>
       <Header>
         <div style={{ display: 'flex', flex: 1 }}>
           <h1>CollabKit</h1>
         </div>
         <div style={{ display: 'flex', flexDirection: 'row', gap: '4rem' }}>
-          <Text>Contact us</Text>
-          <div style={{ position: 'relative', top: '0.5rem' }}>
-            <Button>Request a demo</Button>
-          </div>
+          <Text>
+            <Link href="mailto:namit@collabkit.dev">Contact us</Link>
+          </Text>
+          <div style={{ position: 'relative', top: '0.5rem' }}>{RequestDemoButton}</div>
         </div>
       </Header>
-      <Section>
-        <VStack>
-          <Title>
-            Add commenting <br />
-            to <Em>your</Em> product
-          </Title>
-          <Text style={{ textAlign: 'center' }}>
-            It only takes minutes, with our
-            <br /> customisable React SDK.
-          </Text>
-        </VStack>
-        <img src={ChevronDown} />
-        <BackgroundCenter>
+      <Section
+        style={{
+          position: 'relative',
+          overflow: 'hidden',
+        }}
+      >
+        <Grid
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            columnGap: '0',
+          }}
+        >
+          <Circle color="sky" style={{ transform: 'translateX(-33%)' }} />
           <Circle
-            color="sky"
-            style={{ position: 'absolute', left: 0, transform: 'translateX(-66%)' }}
-          />
-          <Circle color="yellow" />
-          <Circle
-            color="purple"
-            style={{ position: 'absolute', right: 0, transform: 'translateX(66%)' }}
-          />
-        </BackgroundCenter>
+            color="yellow"
+            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          >
+            <VStack style={{ margin: '0 -5rem' }}>
+              <Title>
+                Add commenting to <br />
+                <Em>your</Em> product
+              </Title>
+              <Text style={{ textAlign: 'center' }}>
+                It only takes minutes, with our
+                <br /> customisable React SDK.
+              </Text>
+            </VStack>
+          </Circle>
+          <Circle color="purple" style={{ transform: 'translateX(33%)' }} />
+        </Grid>
+        <img
+          src={ChevronDown}
+          style={{
+            position: 'absolute',
+            left: '50%',
+            marginLeft: -14,
+            bottom: 'calc((100vh - 40rem)/4)',
+          }}
+        />
       </Section>
-      <Section style={{ background: '#9FEFD7' }}>
-        <HStack style={{ gap: '4rem' }}>
-          <img src={Example} />
-          <VStack style={{ width: 320 }}>
+      <Section
+        style={{
+          background: '#9FEFD7',
+        }}
+      >
+        <div
+          style={{
+            display: 'grid',
+            justifyContent: 'center',
+            gridTemplateColumns: 'minmax(20rem, 60rem) 20rem',
+            columnGap: '4rem',
+            width: 'calc(100vw - 4rem)',
+          }}
+        >
+          <img src={Example} style={{ width: '100%' }} />
+          <VStack>
             <Subtitle>
               Boost activation <br />
               and retention
@@ -211,126 +253,135 @@ export function Home() {
               </b>
             </Text>
           </VStack>
-        </HStack>
+        </div>
       </Section>
-      <Section>
-        <Title>
-          Have <br />
-          conversations
-          <br /> with <Em>context</Em>
-        </Title>
-        <Text style={{ textAlign: 'center' }}>
-          CollabKit works just like commenting in
-          <br /> Figma, Google Docs and Notion.
-        </Text>
-        <BackgroundCenter>
+      <Section
+        style={{
+          position: 'relative',
+          overflow: 'hidden',
+        }}
+      >
+        <Grid
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            columnGap: '0',
+          }}
+        >
+          <Circle color="pink" style={{ transform: 'translateX(-33%)' }} />
           <Circle
-            color="pink"
-            style={{ position: 'absolute', left: 0, transform: 'translateX(-66%)' }}
-          />
-          <Circle color="amber" />
-          <Circle
-            color="indigo"
-            style={{ position: 'absolute', right: 0, transform: 'translateX(66%)' }}
-          />
-        </BackgroundCenter>
+            color="amber"
+            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          >
+            <VStack style={{ margin: '0 -5rem' }}>
+              <Title>
+                Have conversations <br />
+                with <Em>context</Em>
+              </Title>
+              <Text style={{ textAlign: 'center' }}>
+                CollabKit works just like commenting in
+                <br /> Figma, Google Docs and Notion.
+              </Text>
+            </VStack>
+          </Circle>
+          <Circle color="indigo" style={{ transform: 'translateX(33%)' }} />
+        </Grid>
       </Section>
-      <Section>
+      <Section style={{ justifyContent: 'center', alignItems: 'center' }}>
         <Article>
-          <VStack style={{ maxWidth: '380px' }}>
+          <VStack>
             <Subtitle>Comment on anything</Subtitle>
             <Text>
               Have discussions in context. Conversations about that sale, product, invoice or
               customer all happen in the same place.
             </Text>
           </VStack>
-          <div style={{ width: 440 }}>
+          <div style={{ width: '100%' }}>
             <img src={CommentAnything} />
           </div>
         </Article>
         <Article>
-          <VStack style={{ maxWidth: '380px' }}>
+          <VStack>
             <Subtitle>In any type of app</Subtitle>
             <Text>
               CollabKit Works across all kinds of interfaces; CRMs, documents, analytics dashboards,
               calendars and more...
             </Text>
           </VStack>
-          <div style={{ width: 440, position: 'relative', left: -44 }}>
-            <img src={AnyApp} />
+          <div style={{ width: '100%', position: 'relative', left: -44 }}>
+            <img src={AnyApp} style={{ width: '107%' }} />
           </div>
         </Article>
         <Article>
-          <VStack style={{ maxWidth: '380px' }}>
+          <VStack>
             <Subtitle>All in real time</Subtitle>
             <Text>
               See who’s online, who’s typing and keep the conversation going without ever leaving
               your product.
             </Text>
           </VStack>
-          <div style={{ width: 440 }}>
-            <img src={Realtime} />
+          <div style={{ width: '100%' }}>
+            <img src={Realtime} style={{ width: '87%' }} />
           </div>
         </Article>
         <Article>
-          <VStack style={{ maxWidth: '380px' }}>
+          <VStack>
             <Subtitle>Get notified by email</Subtitle>
             <Text>
               New comments are sent straight to your inbox. Reply directly via email or click
               through to the comment thread.
             </Text>
           </VStack>
-          <div style={{ width: 440 }}>
+          <div style={{ width: '100%' }}>
             <img src={Email} />
           </div>
         </Article>
       </Section>
-      <Section style={{ background: '#343643', color: 'white' }}>
-        <VStack style={{ textAlign: 'center' }}>
+      <Section
+        style={{
+          background: '#343643',
+          color: 'white',
+        }}
+      >
+        <VStack style={{ textAlign: 'center', marginTop: '-8rem' }}>
           <Title style={{ color: 'white' }}>
             Why <Em>build</Em> it?
           </Title>
           <Text>CollabKit React SDK integrates in minutes.</Text>
-          <br />
-          <br />
-          <br />
-          <br />
         </VStack>
-        <HStack style={{ gap: '4rem' }}>
-          <VStack style={{ maxWidth: '380px' }}>
-            <Subtitle>Let us do the hard work</Subtitle>
-
+        <br />
+        <br />
+        <br />
+        <br />
+        <Article>
+          <VStack>
+            <Subtitle>
+              Let us do the <br />
+              hard work
+            </Subtitle>
             <Text>
               Install our customisable React SDK and add real time collaboration to your product in
               no time.
             </Text>
           </VStack>
-          <div
-            style={{
-              // padding: '1rem',
-              // fontSize: '1.5rem',
-              // lineHeight: '1.5rem',
-              // background: 'rgba(0,0,0,0.1)',
-              width: 440,
-              // transform: 'scale(1.5)',
-              // position: 'relative',
-              // left: 200,
-              // top: 80,
-            }}
-          >
+          <div style={{ marginTop: '-16rem' }}>
             <ExampleCode />
           </div>
-        </HStack>
+        </Article>
       </Section>
       <Section style={{ background: '#905EC9' }}>
-        <Title>
-          Get <Em>started</Em> now
-        </Title>
-        <Text style={{ textAlign: 'center' }}>Schedule a chat with our team</Text>
-        <Button>Request a demo</Button>
-        <BackgroundCenter>
-          <Circle color="yellow" />
-        </BackgroundCenter>
+        <Circle color="yellow">
+          <VStack style={{ marginTop: '8rem', justifyContent: 'center' }}>
+            <Title>
+              Get <Em>started</Em> now
+            </Title>
+            <Text style={{ textAlign: 'center' }}>Schedule a chat with our team</Text>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <VStack>{RequestDemoButton}</VStack>
+            </div>
+          </VStack>
+        </Circle>
       </Section>
     </div>
   );
