03c37e62d50f89aea327b4110e78adf855eb0bba
Namit Chadha <namit.chadha@gmail.com>
2022-08-09 11:43:20 +0100

    email test

--- functions/src/sendNotification.ts
+++ functions/src/sendNotification.ts
@@ -3,6 +3,13 @@ import * as functions from 'firebase-functions';
 // on a new event, writes to the notification log
 // the notification log is periodically processed and notifies other users
 
+import * as mailgun from 'mailgun-js';
+const DOMAIN = 'mail.collabkit.dev';
+const mg = mailgun({
+  apiKey: 'add529c2a5dca50c702baf11a82b8ce2-50f43e91-0898e31b',
+  domain: DOMAIN,
+});
+
 exports.sendNotification = functions.database
   .ref('/timeline/{appId}/{workspaceId}/{roomId}/{eventId}')
   .onCreate((snapshot, context) => {
@@ -14,4 +21,14 @@ exports.sendNotification = functions.database
     const { appId, workspaceId, eventId } = context.params;
 
     functions.database.ref(`/profiles/${appId}/${workspaceId}/${event.createdById}/${eventId}/`);
+
+    const data = {
+      from: 'Excited User <me@samples.mailgun.org>',
+      to: 'namit@collabkit.dev',
+      subject: 'Hello',
+      text: 'Testing some Mailgun awesomness!',
+    };
+    mg.messages().send(data, function (_error, body) {
+      console.log(body);
+    });
   });
