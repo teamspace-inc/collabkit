f3e7b35b5e2173b44cd687eed0503b11cd67ddc6
Namit Chadha <namit.chadha@gmail.com>
2022-08-09 12:04:15 +0100

    comment out mailgun for now

--- functions/src/sendNotification.ts
+++ functions/src/sendNotification.ts
@@ -4,12 +4,12 @@ import * as admin from 'firebase-admin';
 // on a new event, writes to the notification log
 // the notification log is periodically processed and notifies other users
 
-import * as mailgun from 'mailgun-js';
-const DOMAIN = 'mail.collabkit.dev';
-const mg = mailgun({
-  apiKey: 'add529c2a5dca50c702baf11a82b8ce2-50f43e91-0898e31b',
-  domain: DOMAIN,
-});
+// import * as mailgun from 'mailgun-js';
+// const DOMAIN = 'mail.collabkit.dev';
+// const mg = mailgun({
+//   apiKey: 'add529c2a5dca50c702baf11a82b8ce2-50f43e91-0898e31b',
+//   domain: DOMAIN,
+// });
 
 export const sendNotification = functions.database
   .ref('/timeline/{appId}/{workspaceId}/{threadId}/{eventId}')
@@ -49,12 +49,12 @@ export const sendNotification = functions.database
         console.error('error retrieving data', err);
       });
 
-    const data = {
-      from: 'Excited User <me@samples.mailgun.org>',
-      to: 'namit@collabkit.dev',
-      subject: 'Hello',
-      text: 'Testing some Mailgun awesomness!',
-    };
+    // const data = {
+    //   from: 'Excited User <me@samples.mailgun.org>',
+    //   to: 'namit@collabkit.dev',
+    //   subject: 'Hello',
+    //   text: 'Testing some Mailgun awesomness!',
+    // };
 
     // mg.messages().send(data, function (_error, body) {
     //   console.log(body);
