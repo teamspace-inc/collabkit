25f5ccb8e207b3b2b2ec7951ade77564e0ee8908
Ville Immonen <ville.immonen@iki.fi>
2022-08-10 13:38:54 +0300

    Fix errors

--- packages/@collabkit/react/src/actions/unloadThread.ts
+++ packages/@collabkit/react/src/actions/unloadThread.ts
@@ -1,6 +1,8 @@
+import { getConfig } from '.';
 import { Store } from '../constants';
-import { timelineRef } from './index';
+import { timelineRef } from '../sync/firebase/refs';
 
 export function unloadThread(store: Store, props: { workspaceId: string; threadId: string }) {
-  store.subs[timelineRef(store, props.workspaceId, props.threadId).toString()]?.();
+  const { appId } = getConfig(store);
+  store.subs[timelineRef(appId, props.workspaceId, props.threadId).toString()]?.();
 }
--- packages/@collabkit/react/src/components/Pin.stories.tsx
+++ packages/@collabkit/react/src/components/Pin.stories.tsx
@@ -1,13 +1,24 @@
-import { nanoid } from 'nanoid';
 import React from 'react';
 import * as CollabKit from '../';
 
 export const Pin = () => {
-  const id = nanoid();
-  return <CollabKit.PurePin hasUnread={false} profile={{ name: 'N' }} isTyping={{}} />;
+  return (
+    <CollabKit.PurePin
+      currentUserId="any-user"
+      hasUnread={false}
+      profile={{ name: 'N' }}
+      isTyping={{}}
+    />
+  );
 };
 
 export const PinTyping = () => {
-  const id = nanoid();
-  return <CollabKit.PurePin hasUnread={false} profile={{ name: 'N' }} isTyping={{ foo: true }} />;
+  return (
+    <CollabKit.PurePin
+      currentUserId="any-user"
+      hasUnread={false}
+      profile={{ name: 'N' }}
+      isTyping={{ foo: true }}
+    />
+  );
 };
