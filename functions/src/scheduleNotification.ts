18291d8c134d1f66badf59c6098592a18a9c0714
Namit Chadha <namit.chadha@gmail.com>
2022-08-12 12:19:16 +0100

    schedule notif

--- functions/src/scheduleNotification.ts
+++ functions/src/scheduleNotification.ts
@@ -1,18 +1,5 @@
 import * as functions from 'firebase-functions';
-// import { generateNotification } from './generateNotification';
-// import * as admin from 'firebase-admin';
 import { v2 } from '@google-cloud/tasks';
-// on a new event, writes to the notification log
-// the notification log is periodically processed and notifies other users
-
-// import * as mailgun from 'mailgun-js';
-// const DOMAIN = 'mail.collabkit.dev';
-// const mg = mailgun({
-//   apiKey: 'add529c2a5dca50c702baf11a82b8ce2-50f43e91-0898e31b',
-//   domain: DOMAIN,
-// });
-
-// .default workspace = single tenant
 
 export const scheduleNotification = functions.database
   .ref('/timeline/{appId}/{workspaceId}/{threadId}/{eventId}')
@@ -53,6 +40,7 @@ export const scheduleNotification = functions.database
         body,
       },
       scheduleTime: {
+        // 5 minutes from now
         seconds: Math.floor((Date.now() + 5 * 60 * 1000) / 1000),
       },
     } as const;
@@ -71,24 +59,4 @@ export const scheduleNotification = functions.database
         return;
       }
     })();
-
-    // generateNotification({ appId, workspaceId, threadId, eventId }, snapshot.val()).then(
-    //   (notification) => {
-    //     console.log('notification', notification);
-    //   }
-    // );
-
-    // const event = snapshot.val();
-    // const { appId, workspaceId, threadId, eventId } = context.params;
-
-    // const data = {
-    //   from: 'Excited User <me@samples.mailgun.org>',
-    //   to: 'namit@collabkit.dev',
-    //   subject: 'Hello',
-    //   text: 'Testing some Mailgun awesomness!',
-    // };
-
-    // mg.messages().send(data, function (_error, body) {
-    //   console.log(body);
-    // });
   });
