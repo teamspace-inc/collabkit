63a9fe14dceab8ad10c4514158463f8032e22941
Namit Chadha <namit.chadha@gmail.com>
2022-08-15 20:12:41 +0100

    secure: false

--- functions/src/emails/index.ts
+++ functions/src/emails/index.ts
@@ -5,7 +5,7 @@ const transport = nodemailer.createTransport({
   pool: true,
   host: 'smtp.mailgun.org',
   port: 587,
-  secure: true, // use TLS
+  secure: false, // use TLS
   auth: {
     user: process.env.MAILER_USER,
     pass: process.env.MAILER_PASSWORD,
