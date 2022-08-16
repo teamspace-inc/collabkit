eb1877eae69ec1acdec6e670dc2514bf8b1e7234
Ville Immonen <ville.immonen@iki.fi>
2022-08-16 15:48:23 +0300

    Setup the Vue package

---
+++ packages/@collabkit/vue/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Thread } from './';
+</script>
+
+<template>
+  <main>
+    <Thread threadId="vue-chat" />
+  </main>
+</template>
+
+<style scoped>
+  main {
+    padding: 1rem;
+  }
+</style>
