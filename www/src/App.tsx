dc409533992186ec54f170d7f0a645ad6c6c7f40
Namit Chadha <namit.chadha@gmail.com>
2022-08-17 17:49:08 +0100

    add intercom

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -3,10 +3,22 @@ import { Route } from 'wouter';
 import { Home } from './home/Home';
 import { Devs } from './devs/Devs';
 import '@code-hike/mdx/dist/index.css';
+import { useEffect } from 'react';
 
 const Page = styled('div', {});
 
 export default function App() {
+  useEffect(() => {
+    // @ts-expect-error
+    window.Intercom('boot', {
+      api_base: 'https://api-iam.intercom.io',
+      app_id: 'cwr7lgni',
+    });
+
+    // @ts-expect-error
+    window.Intercom('update');
+  });
+
   return (
     <Page>
       <Route path="/" component={Home} />
