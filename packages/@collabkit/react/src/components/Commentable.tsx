61ad0569a58623b36e54f73405096e50c1ad2771
Namit Chadha <namit.chadha@gmail.com>
2022-08-17 17:52:05 +0100

    remove useWorkspace hook

--- packages/@collabkit/react/src/components/Commentable.tsx
+++ packages/@collabkit/react/src/components/Commentable.tsx
@@ -4,13 +4,12 @@ import { useSnapshot } from 'valtio';
 import { Pin } from './Pin';
 import { finder } from '@medv/finder';
 import { Sticky } from './Sticky';
-import { useWorkspace } from '../hooks/useWorkspace';
 
 export function Commentable(props: { children: React.ReactNode; style?: React.CSSProperties }) {
   const { store, events } = useApp();
-  const { uiState, viewingId, hoveringId } = useSnapshot(store);
+  const { uiState, viewingId, hoveringId, workspaceId, workspaces } = useSnapshot(store);
   const ref = useRef<HTMLElement>(null);
-  const { workspace, workspaceId } = useWorkspace();
+  const workspace = workspaceId ? workspaces[workspaceId] : null;
   const pinIds = Object.keys(workspace?.pins || {});
 
   return (
@@ -45,36 +44,42 @@ export function Commentable(props: { children: React.ReactNode; style?: React.CS
           }
         }
 
-        const target = {
-          type: 'commentable',
-          workspaceId,
-          pin: {
-            selector,
-            offset: { x, y },
-            url: window.location.href.toString(),
-          },
-        } as const;
+        const target = workspaceId
+          ? ({
+              type: 'commentable',
+              workspaceId,
+              pin: {
+                selector,
+                offset: { x, y },
+                url: window.location.href.toString(),
+              },
+            } as const)
+          : null;
 
-        events.onPointerDown(e, { target });
+        if (target) {
+          events.onPointerDown(e, { target });
+        }
       }}
     >
       {props.children}
-      {pinIds.map((pinId) => {
-        const pin = workspace.pins[pinId];
-        const isHovering = hoveringId?.type === 'pin' && hoveringId.pinId === pinId;
-        const isViewing = viewingId?.type === 'pin' && viewingId?.pinId === pinId;
+      {workspace
+        ? pinIds.map((pinId) => {
+            const pin = workspace.pins[pinId];
+            const isHovering = hoveringId?.type === 'pin' && hoveringId.pinId === pinId;
+            const isViewing = viewingId?.type === 'pin' && viewingId?.pinId === pinId;
 
-        return pin.state !== 'resolved' ? (
-          <Sticky
-            key={pinId}
-            selector={pin.selector}
-            offset={pin.offset}
-            zTop={isViewing || isHovering}
-          >
-            <Pin pinId={pinId} />
-          </Sticky>
-        ) : null;
-      })}
+            return pin.state !== 'resolved' ? (
+              <Sticky
+                key={pinId}
+                selector={pin.selector}
+                offset={pin.offset}
+                zTop={isViewing || isHovering}
+              >
+                <Pin pinId={pinId} />
+              </Sticky>
+            ) : null;
+          })
+        : null}
     </span>
   );
 }
--- packages/@collabkit/react/src/components/Inbox.tsx
+++ packages/@collabkit/react/src/components/Inbox.tsx
@@ -4,7 +4,6 @@ import React, { useEffect } from 'react';
 import { useSnapshot } from 'valtio';
 import { actions } from '../actions';
 import { useApp } from '../hooks/useApp';
-import { useWorkspace } from '../hooks/useWorkspace';
 
 const StyledInbox = styled('div', inboxStyles.inbox);
 const StyledInboxItem = styled('div', inboxStyles.inboxItem);
@@ -13,10 +12,10 @@ const StyledThreadMessagePreview = styled('div', inboxStyles.threadMessagePrevie
 
 export function Inbox() {
   const { store } = useApp();
-  const { profiles, appState } = useSnapshot(store);
-  const { workspace } = useWorkspace();
+  const { profiles, appState, workspaceId, workspaces } = useSnapshot(store);
 
-  const inboxIds = Object.keys(workspace.inbox);
+  const workspace = workspaceId ? workspaces[workspaceId] : null;
+  const inboxIds = workspace ? Object.keys(workspace.inbox) : null;
 
   useEffect(() => {
     if (appState !== 'ready') return;
@@ -26,22 +25,28 @@ export function Inbox() {
   return inboxIds ? (
     <div style={{ width: '100%' }}>
       <StyledInbox style={{ width: '100%', maxHeight: 500, overflow: 'scroll' }}>
-        {inboxIds
-          .sort((a, b) => (workspace.inbox[a].createdAt <= workspace.inbox[b].createdAt ? 1 : -1))
-          .map((threadId) => {
-            const event = workspace.inbox[threadId];
-            const profile = profiles[event?.createdById || ''];
-            const seen = workspace.seen[threadId];
-            const isUnread = seen < event.id;
-            return (
-              <StyledInboxItem key={threadId} isUnread={isUnread}>
-                <StyledThreadName isUnread={isUnread}>{event?.name || 'Unnamed'}</StyledThreadName>
-                <StyledThreadMessagePreview isUnread={isUnread}>
-                  {profile?.name}: {event?.body}
-                </StyledThreadMessagePreview>
-              </StyledInboxItem>
-            );
-          })}
+        {workspace
+          ? inboxIds
+              .sort((a, b) =>
+                workspace.inbox[a].createdAt <= workspace.inbox[b].createdAt ? 1 : -1
+              )
+              .map((threadId) => {
+                const event = workspace.inbox[threadId];
+                const profile = profiles[event?.createdById || ''];
+                const seen = workspace.seen[threadId];
+                const isUnread = seen < event.id;
+                return (
+                  <StyledInboxItem key={threadId} isUnread={isUnread}>
+                    <StyledThreadName isUnread={isUnread}>
+                      {event?.name || 'Unnamed'}
+                    </StyledThreadName>
+                    <StyledThreadMessagePreview isUnread={isUnread}>
+                      {profile?.name}: {event?.body}
+                    </StyledThreadMessagePreview>
+                  </StyledInboxItem>
+                );
+              })
+          : null}
       </StyledInbox>
     </div>
   ) : (
