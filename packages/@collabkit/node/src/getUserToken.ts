232599b126fa55e4f55e505b1fd1cfb565533f3d
Namit Chadha <namit.chadha@gmail.com>
2022-08-18 13:03:55 +0100

    added @collabkit/node#getUserToken for secure-mode

---
+++ packages/@collabkit/node/src/getUserToken.ts
@@ -0,0 +1,42 @@
+import { UserProps, WorkspaceProps } from '@collabkit/core';
+
+export async function getUserToken(props: {
+  appId: string;
+  apiKey: string;
+  workspaceId: string;
+  userId: string;
+  user: UserProps;
+  workspace: WorkspaceProps;
+}): Promise<
+  | {
+      status: 201;
+      data: {
+        appId: string;
+        mode: 'SECURED';
+        token: string;
+        workspaceId: string;
+        userId: string;
+      };
+    }
+  | { status: 400; error: string }
+  | null
+> {
+  const credentials = {
+    mode: 'SECURED',
+    ...props,
+  };
+
+  const response = await fetch(`https://token.collabkit.dev`, {
+    method: 'POST',
+    body: JSON.stringify(credentials),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    return await response.json();
+  }
+
+  return null;
+}
---
+++ packages/@collabkit/node/src/index.ts
@@ -0,0 +1,1 @@
+export { getUserToken } from './getUserToken';
