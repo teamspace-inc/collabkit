cc9fdb6329bbb1250281959b701eaa958c303ddf
Namit Chadha <namit.chadha@gmail.com>
2022-08-18 15:18:48 +0100

    0.1.8

--- examples/next/.next/static/chunks/webpack.js
+++ examples/next/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fb1f399ca233dbcc"; }
+/******/ 		__webpack_require__.h = function() { return "56f44a9b4b9d8ba9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- examples/next/pages/home.tsx
+++ examples/next/pages/home.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import * as CollabKit from '../../../packages/@collabkit/react/src/index';
+import * as CollabKit from '@collabkit/react';
 import { getUserToken } from '@collabkit/node';
 
 export async function getServerSideProps(context: any) {
