ed6f105ecc165a1adebd56aa66e2e75e7cddd9f1
Ville Immonen <ville.immonen@iki.fi>
2022-08-22 15:30:10 +0300

    Delete unused styles / components

--- packages/@collabkit/react/src/components/comment/MessageToolbar.tsx
+++ packages/@collabkit/react/src/components/comment/MessageToolbar.tsx
@@ -1,28 +1,9 @@
-import { styled } from '@stitches/react';
 import React from 'react';
+import { styled } from '@stitches/react';
+import { messageToolbarStyles } from '@collabkit/theme';
 import { ReactionButton } from './ReactionButton';
 
-const StyledMessageToolbar = styled('div', {
-  position: 'absolute',
-  display: 'flex',
-  height: '100%',
-  alignItems: 'flex-start',
-  justifyContent: 'center',
-  top: '4px',
-  right: '10px',
-  variants: {
-    isVisible: {
-      true: {
-        pointerEvents: 'all',
-        opacity: 1,
-      },
-      false: {
-        pointerEvent: 'none',
-        opacity: 0,
-      },
-    },
-  },
-});
+const StyledMessageToolbar = styled('div', messageToolbarStyles.toolbar);
 
 export function MessageToolbar(props: { isVisible: boolean }) {
   return (
--- packages/@collabkit/theme/src/index.ts
+++ packages/@collabkit/theme/src/index.ts
@@ -21,16 +21,9 @@ export * as threadHeaderStyles from './styles/thread/threadHeaderStyles';
 // .
 export * as avatarStyles from './styles/avatarStyles';
 export * as badgeStyles from './styles/badgeStyles';
-export * as buttonStyles from './styles/buttonStyles';
 export * as commentStyles from './styles/commentStyles';
 export * as commentListStyles from './styles/commentListStyles';
 export * as composerStyles from './styles/composerStyles';
-export * as currentUserStyles from './styles/currentUserStyles';
-export * as floatingButtonStyles from './styles/floatingButtonStyles';
 export * as iconButtonStyles from './styles/iconButtonStyles';
-export * as inboxStyles from './styles/inboxStyles';
 export * as mentionsPluginStyles from './styles/mentionsPluginStyles';
-export * as pinStyles from './styles/pinStyles';
-export * as popoverThreadStyles from './styles/popoverThreadStyles';
 export * as scrollAreaStyles from './styles/scrollAreaStyles';
-export * as stickyStyles from './styles/stickyStyles';
