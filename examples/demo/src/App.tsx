6c7a0190843f8f4ce94c8914cade518b250c785a
Namit Chadha <namit.chadha@gmail.com>
2022-08-24 10:54:26 +0100

    tella & dart themes

--- examples/demo/src/App.tsx
+++ examples/demo/src/App.tsx
@@ -47,6 +47,44 @@ export default function App() {
     return image;
   }
 
+  const tella = {
+    radii: { 0: '0.5rem' },
+    fontSize: { 0: '12px', 2: '14px', 3: '20px' },
+    fontWeights: { 2: 500, 3: 700 },
+    colors: {
+      sendButtonColor: 'rgb(94, 81, 248)',
+      backgroundColor: 'rgb(249,249,250)',
+      composerBackground: 'white',
+    },
+    offsets: {
+      composerSendButtonTop: '13px',
+    },
+  };
+
+  const dart = {
+    radii: { 0: '4px' },
+    fontSize: { 0: '12px', 1: '14px', 2: '14px', 3: '20px' },
+    lineHeights: { 0: '20px', 1: '20px' },
+    fontWeights: { 2: 500, 3: 700 },
+    borders: {
+      composer: '1px solid #404045',
+    },
+    colors: {
+      sendButtonColor: '#414286',
+      backgroundColor: '#1e1e21',
+      composerBackground: '#1e1e21',
+      composerPlaceholder: '#515159',
+      primaryText: 'rgb(212,212,216)',
+      caretColor: 'rgb(212,212,216)',
+      commentHoverBackgroundColor: 'rgba(0,0,0,0.1)',
+      commentUnseenBackgroundColor: '#3F3F45',
+      commentUnseenHoverBackgroundColor: 'rgba(255,255,255,0.1)',
+    },
+    offsets: {
+      composerSendButtonTop: '14px',
+    },
+  };
+
   return (
     <CollabKit.Provider
       colorScheme="light"
@@ -62,29 +100,7 @@ export default function App() {
         console.log('authRequired');
       }}
       user={{ ...user, id: user.userId }}
-      theme={{
-        radii: { 0: '4px' },
-        fontSize: { 0: '12px', 1: '14px', 2: '14px', 3: '20px' },
-        lineHeights: { 0: '20px', 1: '20px' },
-        fontWeights: { 2: 500, 3: 700 },
-        borders: {
-          composer: '1px solid #404045',
-        },
-        colors: {
-          sendButtonColor: '#414286',
-          backgroundColor: '#1e1e21',
-          composerBackground: '#1e1e21',
-          composerPlaceholder: '#515159',
-          primaryText: 'rgb(212,212,216)',
-          caretColor: 'rgb(212,212,216)',
-          commentHoverBackgroundColor: 'rgba(0,0,0,0.1)',
-          commentUnseenBackgroundColor: '#3F3F45',
-          commentUnseenHoverBackgroundColor: 'rgba(255,255,255,0.1)',
-        },
-        offsets: {
-          composerSendButtonTop: '14px',
-        },
-      }}
+      theme={tella}
       mentionableUsers={mentionableUsers}
     >
       <Home />
