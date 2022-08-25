4dfa69e302969d742424484540357b268adcc0a3
Ville Immonen <ville.immonen@iki.fi>
2022-08-25 16:17:01 +0300

    [vue] Roll up type definitions to a single file

---
+++ packages/@collabkit/vue/rollup.config.js
@@ -0,0 +1,11 @@
+import dts from 'rollup-plugin-dts';
+
+const config = [
+  {
+    input: './types/vue/src/index.d.ts',
+    output: [{ file: 'dist/collabkit-vue.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
+
+export default config;
