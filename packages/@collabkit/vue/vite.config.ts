ab3abe843f58bbdb23c56f61645e6dd8272e91d2
Ville Immonen <ville.immonen@iki.fi>
2022-08-25 15:43:33 +0300

    [vue] Add build config using Vite lib mode

--- packages/@collabkit/vue/vite.config.ts
+++ packages/@collabkit/vue/vite.config.ts
@@ -1,8 +1,28 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'CollabKit',
+      // the proper extensions will be added
+      fileName: 'collabkit-vue',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 });
