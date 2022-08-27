d650c5e32f473513a55af42aa987cd257bfc3a3c
Ville Immonen <ville.immonen@iki.fi>
2022-08-27 21:25:24 +0300

    Fix

--- packages/@collabkit/react/src/components/icons.ts
+++ packages/@collabkit/react/src/components/icons.ts
@@ -1,3 +1,4 @@
+import '../phosphor-react.d.ts';
 export { default as ArrowUp } from 'phosphor-react/dist/icons/ArrowUp.esm.js';
 export { default as Chats } from 'phosphor-react/dist/icons/Chats.esm.js';
 export { default as CheckCircle } from 'phosphor-react/dist/icons/CheckCircle.esm.js';
