4a1bccf5cda9e9e5343055972105a66404a241f2
Ville Immonen <ville.immonen@iki.fi>
2022-08-29 14:02:39 +0300

    [react] Export 'CollabKitProvider'

--- packages/@collabkit/react/src/index.ts
+++ packages/@collabkit/react/src/index.ts
@@ -1,13 +1,20 @@
+import type {
+  Config,
+  Mention,
+  MentionProps,
+  Store,
+  Subscriptions,
+  Workspace,
+} from '@collabkit/core';
 import type { CustomTheme } from '@collabkit/theme';
 import { CollabKitProvider } from './components/Provider';
 import { Thread } from './components/Thread';
-import { Config, Mention, Store, Subscriptions, Workspace } from './constants';
-import { MentionProps } from './constants';
-import { createValtioStore } from './store';
 import { useUnreadCount } from './hooks/public/useUnreadCount';
+import { createValtioStore } from './store';
 
 export {
   CollabKitProvider as Provider,
+  CollabKitProvider,
   Thread,
   useUnreadCount,
   createValtioStore as internal_createStore,
