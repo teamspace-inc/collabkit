42a0a3aae09657d15c41842290a18452d949e617
Namit Chadha <namit.chadha@gmail.com>
2022-08-29 16:43:59 +0100

    fix email checking regex

--- functions/src/actions/generateAndSendEmailNotifications.tsx
+++ functions/src/actions/generateAndSendEmailNotifications.tsx
@@ -198,7 +198,11 @@ function canSendEmail(profile: Profile) {
   return (
     profile.email &&
     typeof profile.email === 'string' &&
-    profile.email.match(/^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/)
+    profile.email
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
   );
 }
 
