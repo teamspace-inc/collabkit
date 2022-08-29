5400ff49cd167d002e7905591771870846ea083c
Namit Chadha <namit.chadha@gmail.com>
2022-08-29 20:12:59 +0100

    refactor email sending

--- functions/src/actions/generateAndSendEmailNotifications.tsx
+++ functions/src/actions/generateAndSendEmailNotifications.tsx
@@ -16,6 +16,94 @@ import { isThreadInfo } from './helpers/isThreadInfo';
 import { canSendEmail } from './helpers/canSendEmail';
 import { groupedEvents } from './helpers/groupedEvents';
 
+type Data = Exclude<Awaited<ReturnType<typeof fetchData>>, null>;
+
+function setNotifiedUntil(props: {
+  appId: string;
+  workspaceId: string;
+  threadId: string;
+  profileId: string;
+  eventId: string;
+}) {
+  const db = admin.database();
+  return db
+    .ref(`/notifiedUntil/${props.appId}/${props.workspaceId}/${props.profileId}`)
+    .set(props.eventId);
+}
+
+async function fetchProfileSpecificData(props: {
+  data: Data;
+  appId: string;
+  threadId: string;
+  workspaceId: string;
+  profileId: string;
+}) {
+  const { appId, workspaceId, profileId, data } = props;
+  if (!data.profiles[profileId]) {
+    console.debug('no profile found skipping', profileId);
+    return null;
+  }
+
+  if (!data.profiles[profileId].email) {
+    console.debug('no profile email found skipping', profileId);
+    return null;
+  }
+
+  if (!canSendEmail(data.profiles[profileId])) {
+    console.debug('cant send email', profileId);
+    return null;
+  }
+
+  const db = admin.database();
+  const notifiedUntil = (
+    await db.ref(`/notifiedUntil/${appId}/${workspaceId}/${profileId}`).get()
+  ).val();
+
+  if (!isValidNotifiedUntilId(notifiedUntil)) {
+    console.debug('invalid notifiedUntil, skipping', notifiedUntil);
+    return null;
+  }
+
+  let notifyFrom: string | null = null;
+
+  const eventIds = Object.keys(data.timeline);
+
+  // if we've never notified this user, lets start
+  // from the first eventId
+  if (!notifiedUntil) {
+    notifyFrom = eventIds[0];
+  } else if (notifiedUntil) {
+    notifyFrom = eventIds[eventIds.indexOf(notifiedUntil) + 1];
+  }
+
+  if (!notifyFrom) {
+    console.log('no notifyFrom, skipping', profileId);
+    return null;
+  }
+
+  const newerEventIds = eventIds.slice(eventIds.indexOf(notifyFrom) + 1);
+
+  if (newerEventIds.length === 0) {
+    console.debug('no newer events, skipping');
+    return null;
+  }
+  // TODO copy-paste from useTimeline
+  // would be great to use the same logic here
+  const messageEvents = newerEventIds
+    .map((eventId) => data.timeline[eventId])
+    .filter((event) => event.type === 'message');
+
+  const list = groupedEvents(messageEvents, data.timeline);
+
+  return {
+    notifiedUntil,
+    notifyFrom,
+    list,
+    messageEvents,
+    newerEventIds,
+  };
+}
+
 async function fetchData(props: {
   appId: string;
   workspaceId: string;
@@ -27,10 +115,7 @@ async function fetchData(props: {
   const bucket = storage.bucket('collabkit-dev-emails');
   const { appId, workspaceId, threadId, eventId } = props;
 
-  console.log('generateNotification', { appId, workspaceId, threadId, eventId });
-
-  // app name
-  // app logo
+  console.log('fetchData', { appId, workspaceId, threadId, eventId });
 
   const appQuery = db.ref(`/apps/${appId}/`).get();
 
@@ -143,8 +228,6 @@ async function fetchData(props: {
         timelineWithEventIds[eventId] = { ...timeline[eventId], id: eventId };
       }
 
-      // const eventIds = Object.keys(timelineWithEventIds);
-
       let _event = timelineWithEventIds[eventId];
 
       const lastEventId = Object.keys(timelineWithEventIds)[Object.keys(timeline).length - 1];
@@ -168,7 +251,7 @@ async function fetchData(props: {
         profiles,
         seenBy,
         timeline: timelineWithEventIds,
-        threadInfo: threadInfoSnapshot.val(),
+        threadInfo,
         eventId,
         event: _event,
         lastEventId,
@@ -184,220 +267,164 @@ async function fetchData(props: {
   return null;
 }
 
+async function sendMailForProfile(props: {
+  eventId: string;
+  appId: string;
+  workspaceId: string;
+  profileId: string;
+  threadId: string;
+  data: Data;
+}) {
+  const { eventId, appId, workspaceId, profileId, threadId, data } = props;
+  const profileData = await fetchProfileSpecificData({
+    appId,
+    workspaceId,
+    threadId,
+    profileId,
+    data,
+  });
+
+  if (!profileData) {
+    return null;
+  }
+
+  const { newerEventIds, list } = profileData;
+
+  if (profileId === data.event.createdById) {
+    console.debug(
+      'profileId === _event.createdById skipping and setting notifiedUntil',
+      profileId,
+      eventId
+    );
+    await setNotifiedUntil({ appId, workspaceId, threadId, profileId, eventId });
+    return null;
+  }
+
+  const to = data.profiles[profileId].email;
+  // const from = 'noreply@mail.collabkit.dev';
+
+  const threadName = data.threadInfo.name;
+
+  // there's a bug here.
+  const subject =
+    newerEventIds.length === 1 ? `New comment on ${threadName}` : `New comments on ${threadName}`;
+
+  const component = (
+    <NotificationEmail
+      openUrl={data.threadInfo.url}
+      accentColor={data.app.accentColor}
+      appLogoUrl={data.app.logoUrl}
+      ctaText={list.length === 1 ? 'View comment' : 'View comments'}
+      activity={list.length === 1 ? 'New comment' : 'New comments'}
+      threadName={data.threadInfo.name}
+      workspaceName={data.workspace.name}
+      productName={data.app.name}
+      commentList={list}
+      profiles={data.profiles}
+    />
+  );
+
+  const mail = {
+    subject,
+    to,
+    component,
+  };
+
+  console.log(mail.subject, mail.to);
+
+  const newNotifiedUntilId = newerEventIds[newerEventIds.length - 1];
+
+  if (!newNotifiedUntilId) {
+    console.debug('no newNotifiedUntilId, exiting');
+    return null;
+  }
+
+  try {
+    await sendMail(mail);
+  } catch (e) {
+    console.error('sendMail failed', e);
+    return null;
+  }
+
+  return setNotifiedUntil({
+    appId,
+    workspaceId,
+    profileId,
+    threadId,
+    eventId: newNotifiedUntilId,
+  });
+}
+
 export async function generateAndSendEmailNotifications(props: {
   appId: string;
   workspaceId: string;
   threadId: string;
   eventId: string;
 }) {
-  const db = admin.database();
-  const storage = admin.storage();
-  // const bucket = storage.bucket('collabkit-dev-emails');
   const { appId, workspaceId, threadId, eventId } = props;
 
   console.log('generateNotification', { appId, workspaceId, threadId, eventId });
 
-  try {
-    const data = await fetchData(props);
-    if (data === null) {
-      return;
-    }
-
-    const {
-      app,
-      workspace,
-      profiles,
-      // seenBy,
-      event,
-      timeline,
-      threadInfo,
-      eventId,
-      // lastEventId,
-      profileIds,
-      // actorProfile,
-      // isFirstEvent,
-    } = data;
-
-    const eventIds = Object.keys(timeline);
-
-    // for workspaces we want to notify everyone in the workspace for
-    // new comments note we probably want to auto-mute this at 50+ workspace members
-    // otherwise things could get a bit too spammy
-
-    // email notifs for tella go here
-    if (workspaceId.toLowerCase() === 'default') {
-      // to notify:
-      // + everyone in the thread
-      // + everyone who has been mentioned
-      // - anyone who has muted the thread
-      //// TODO
-      // const toNotify: string[] = events
-      //   .map((event: { createdById: string }) => event.createdById)
-      //   // unique
-      //   .filter((v: any, i: any, a: string | any[]) => a.indexOf(v) === i)
-      //   // remove creator
-      //   .filter((id: string) => id !== actorProfile.id);
-      // console.debug('toNotify', toNotify);
-      // for (const profileId of toNotify) {
-      //   try {
-      //     const notifiedUntil = (
-      //       await db
-      //         .ref(`/notifiedUntil/${appId}/${workspaceId}/${threadId}/${profileId}`)
-      //         .get()
-      //     ).val();
-      //     if (notifiedUntil && notifiedUntil >= eventId) {
-      //       console.debug('skip: already notified', profileId);
-      //     }
-      //   } catch (e) {
-      //     console.error('error getting notifiedUntil, aborting', e);
-      //   }
-      // }
-    } else {
-      // to notify:
-      // + everyone in the workspace
-      // + everyone who has been mentioned
-      // - anyone who has muted the workspace
-      // - anyone who has muted the thread
-
-      const threadName = threadInfo.name;
-
-      for (const profileId of profileIds) {
-        if (!profiles[profileId]) {
-          console.debug('no profile found skipping', profileId);
-          continue;
-        }
-
-        if (!profiles[profileId].email) {
-          console.debug('no profile email found skipping', profileId);
-          continue;
-        }
-
-        if (!canSendEmail(profiles[profileId])) {
-          console.debug('cant send email', profileId);
-          continue;
-        }
-
-        if (profileId === event.createdById) {
-          console.debug(
-            'profileId === _event.createdById skipping and setting notifiedUntil',
-            profileId,
-            eventId
-          );
-          await db.ref(`/notifiedUntil/${appId}/${workspaceId}/${profileId}`).set(eventId);
-          continue;
-        }
-
-        try {
-          const notifiedUntil = (
-            await db.ref(`/notifiedUntil/${appId}/${workspaceId}/${profileId}`).get()
-          ).val();
-
-          if (!isValidNotifiedUntilId(notifiedUntil)) {
-            console.debug('invalid notifiedUntil, skipping', notifiedUntil);
-            continue;
-          }
-
-          let notifyFrom: string | null = null;
-
-          // if we've never notified this user, lets start
-          // from the first eventId
-          if (!notifiedUntil) {
-            notifyFrom = eventIds[0];
-          } else if (notifiedUntil) {
-            notifyFrom = eventIds[eventIds.indexOf(notifiedUntil) + 1];
-          }
-
-          if (!notifyFrom) {
-            console.log('no notifyFrom, skipping', profileId);
-            continue;
-          }
-
-          const newerEventIds = eventIds.slice(eventIds.indexOf(notifyFrom) + 1);
-
-          if (newerEventIds.length === 0) {
-            console.debug('no newer events, skipping');
-            continue;
-          }
-
-          const to = profiles[profileId].email;
-          // const from = 'noreply@mail.collabkit.dev';
-          const subject =
-            newerEventIds.length === 1
-              ? `New comment on ${threadName}`
-              : `New comments on ${threadName}`;
-
-          // TODO copy-paste from useTimeline
-          // would be great to use the same logic here
-          const messageEvents = newerEventIds
-            .map((eventId) => timeline[eventId])
-            .filter((event) => event.type === 'message');
-
-          const list = groupedEvents(messageEvents, timeline);
-
-          if (!threadInfo.url) {
-            console.debug('no thread url, skipping', threadInfo);
-            continue;
-          }
-
-          const component = (
-            <NotificationEmail
-              openUrl={threadInfo.url}
-              accentColor={app.accentColor}
-              appLogoUrl={app.logoUrl}
-              ctaText={list.length === 1 ? 'View comment' : 'View comments'}
-              activity={list.length === 1 ? 'New comment' : 'New comments'}
-              threadName={threadName}
-              workspaceName={workspace.name}
-              productName={app.name}
-              commentList={list}
-              profiles={profiles}
-            />
-          );
-
-          const mail = {
-            subject,
-            to,
-            component,
-          };
-
-          console.log(mail.subject, mail.to);
-
-          try {
-            // const file = bucket.file(
-            //   `/emails/${appId}/${workspaceId}/${profileId}/${eventId}.html`
-            // );
-            // tasks[profileId] ||= [
-            // file.save(render(mail.component).html, {
-            //   gzip: true,
-            //   contentType: 'text/html',
-            // }),
-            // db
-            //   .ref(`/emails/${appId}/${workspaceId}/${threadId}/${profileId}/${eventId}`)
-            //   .set({ subject, to }),
-            await sendMail(mail);
-            // ];
-          } catch (e) {
-            console.error('error setting up mail promises', e);
-            return;
-          }
-
-          try {
-            await db
-              .ref(`/notifiedUntil/${appId}/${workspaceId}/${profileId}`)
-              .set(newerEventIds[newerEventIds.length - 1]);
-
-            console.log('notified until', eventId, newerEventIds[newerEventIds.length - 1]);
-          } catch (e) {
-            console.error('error setting notifiedUntil, aborting', e);
-          }
-        } catch (e) {
-          console.error('error getting notifiedUntil, aborting', e);
-        }
-      }
-    }
-  } catch (e) {
-    console.error('error fetching profiles', e);
+  const data = await fetchData(props);
+  if (data === null) {
     return;
+  }
+
+  const { profileIds } = data;
+
+  // for workspaces we want to notify everyone in the workspace for
+  // new comments note we probably want to auto-mute this at 50+ workspace members
+  // otherwise things could get a bit too spammy
+
+  // email notifs for tella go here
+  if (workspaceId.toLowerCase() === 'default') {
+    // to notify:
+    // + everyone in the thread
+    // + everyone who has been mentioned
+    // - anyone who has muted the thread
+    //// TODO
+    // const toNotify: string[] = events
+    //   .map((event: { createdById: string }) => event.createdById)
+    //   // unique
+    //   .filter((v: any, i: any, a: string | any[]) => a.indexOf(v) === i)
+    //   // remove creator
+    //   .filter((id: string) => id !== actorProfile.id);
+    // console.debug('toNotify', toNotify);
+    // for (const profileId of toNotify) {
+    //   try {
+    //     const notifiedUntil = (
+    //       await db
+    //         .ref(`/notifiedUntil/${appId}/${workspaceId}/${threadId}/${profileId}`)
+    //         .get()
+    //     ).val();
+    //     if (notifiedUntil && notifiedUntil >= eventId) {
+    //       console.debug('skip: already notified', profileId);
+    //     }
+    //   } catch (e) {
+    //     console.error('error getting notifiedUntil, aborting', e);
+    //   }
+    // }
+  } else {
+    // to notify:
+    // + everyone in the workspace
+    // + everyone who has been mentioned
+    // - anyone who has muted the workspace
+    // - anyone who has muted the thread
+
+    // for all the profiles in this workspace,
+    // generate and send an email notification
+    await Promise.allSettled(
+      profileIds.map(async (profileId) => {
+        return sendMailForProfile({
+          eventId,
+          appId,
+          workspaceId,
+          profileId,
+          threadId,
+          data,
+        });
+      })
+    );
   }
 
   return null;
