6177fa609c542fa35c9768607632f5c66bb3eddc
Ville Immonen <ville.immonen@iki.fi>
2022-08-29 13:58:43 +0300

    Delete CollabKit.Debug

--- packages/@collabkit/react/src/index.ts
+++ packages/@collabkit/react/src/index.ts
@@ -1,6 +1,5 @@
 import type { CustomTheme } from '@collabkit/theme';
 import { CollabKitProvider } from './components/Provider';
-import { Debug } from './components/Debug';
 import { Thread } from './components/Thread';
 import { Config, Mention, Store, Subscriptions, Workspace } from './constants';
 import { MentionProps } from './constants';
@@ -10,10 +9,7 @@ import { useUnreadCount } from './hooks/public/useUnreadCount';
 export {
   CollabKitProvider as Provider,
   Thread,
-  Debug,
-  // withComments,
   useUnreadCount,
-  // pure components just render props passed
   createValtioStore as internal_createStore,
 };
 export type { Config, Mention, MentionProps, Store, Subscriptions, CustomTheme, Workspace };
