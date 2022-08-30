70b41ea702b1746bb0266e00b24502c2ab758b0e
Ville Immonen <ville.immonen@iki.fi>
2022-08-30 13:41:02 +0300

    [vue] Expose `Profile` type (useful for avatar slot components)

--- packages/@collabkit/vue/src/index.ts
+++ packages/@collabkit/vue/src/index.ts
@@ -2,4 +2,5 @@ export { default as CollabKitProvider } from './components/Provider.vue';
 export { default as Thread } from './components/Thread.vue';
 export { useUnreadCount } from './composables/useUnreadCount';
 export type { CustomTheme } from '@collabkit/theme';
+export type { Profile } from '@collabkit/core';
 export type { AvatarProps } from './types';
