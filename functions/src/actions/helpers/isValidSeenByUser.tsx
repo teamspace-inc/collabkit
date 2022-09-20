bf30b3769b6f9d8503663a919843a58e8e5af99c
Namit Chadha <namit.chadha@gmail.com>
2022-09-20 09:36:18 +0100

    isValidSeenByUser

--- functions/src/actions/helpers/isValidSeenByUser.tsx
+++ functions/src/actions/helpers/isValidSeenByUser.tsx
@@ -2,11 +2,16 @@ import { SeenByUser } from '../../types';
 import has from 'has';
 
 export function isValidSeenByUser(data: any): data is SeenByUser | null {
-  return typeof data === 'object' && data === null
-    ? true
-    : typeof data === 'object' &&
-        'seenUntilId' in data &&
-        'seenAt' in data &&
-        typeof data.seenUntilId === 'string' &&
-        typeof data.seenAt === 'number';
+  if (data === null) {
+    return true;
+  }
+
+  if (typeof data !== 'object') {
+    return false;
+  }
+
+  const userIdValid = has(data, 'seenUntilId') && typeof data.seenUntilId === 'string';
+  const seenAtValid = has(data, 'seenAtValid') && typeof data.seenAtValid === 'number';
+
+  return userIdValid && seenAtValid;
 }
