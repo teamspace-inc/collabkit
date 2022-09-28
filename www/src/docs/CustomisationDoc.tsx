d71250957306ca18f2b0414b8e7648506bd164c1
Namit Chadha <namit.chadha@gmail.com>
2022-09-27 20:38:01 -0700

    customisation doc

--- www/src/docs/CustomisationDoc.tsx
+++ www/src/docs/CustomisationDoc.tsx
@@ -1,5 +1,5 @@
 import { Thread } from '@collabkit/react';
-import { H2 } from '../UIKit';
+import { H2, H3 } from '../UIKit';
 import { CodeEditor, renderCodeSnippet } from './CodeEditor';
 import { DocDemoContainer } from './Doc';
 import { ThemeEditor } from './ThemeEditor';
@@ -8,25 +8,26 @@ export function CustomisationDoc() {
   return (
     <div>
       <H2>Customise the look and feel of CollabKit to entirely match your apps UI.</H2>
+      <H3>Built-in themes</H3>
       <p>
         CollabKit comes with two builtin themes. Light and Dark. Use these as a starting point for
         your own theme.
       </p>
-      <DocDemoContainer>
-        <div style={{ display: 'flex', gap: '12px' }}>
-          <div style={{ display: 'flex', gap: '12px' }}>
-            <span style={{ fontSize: '24px', color: 'white' }}>Built-in themes</span>
-            <div style={{ width: 240, height: 320 }}>
-              Light
-              <Thread threadId={'bar'}></Thread>
-            </div>
-            <div style={{ width: 240, height: 320 }}>
-              Dark
-              <Thread threadId={'foo'}></Thread>
-            </div>
+      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+        <DocDemoContainer>
+          Light
+          <div style={{ width: 320, height: 400 }}>
+            <Thread threadId={'bar'}></Thread>
           </div>
-        </div>
-      </DocDemoContainer>
+        </DocDemoContainer>
+        <DocDemoContainer>
+          Dark
+          <div style={{ width: 320, height: 400 }}>
+            <Thread threadId={'foo'}></Thread>
+          </div>
+        </DocDemoContainer>
+      </div>
+      <H3>Custom themes</H3>
       <p>
         Try editing this custom theme to see how you can customise CollabKit. Note theme variables
         are all type-safe making it easy to edit. You can adjust style variables to change colours,
@@ -47,18 +48,6 @@ export function CustomisationDoc() {
         components that make up a Thread, Popover Thread or Inbox. You can use it to create entirely
         new commenting or collaboration experiences.
       </blockquote>
-      <h3>Demo</h3>
-      <h3>Usage</h3>
-      {renderCodeSnippet(`import type { CustomTheme } from '@collabkit/react';
-import { Provider } from '@collabkit/react';
-
-const theme: CustomTheme = {
-	colors: {
-    buttonPrimaryBackground: '#36B374',
-  }
-}
-
-<Provider theme={theme} />`)}
     </div>
   );
 }
