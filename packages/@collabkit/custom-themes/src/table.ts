4ae82fe334794f905a96aee8f7f754db330c722d
Namit Chadha <namit.chadha@gmail.com>
2022-09-30 19:46:40 -0700

    0.4.4

--- packages/@collabkit/custom-themes/src/table.ts
+++ packages/@collabkit/custom-themes/src/table.ts
@@ -4,7 +4,14 @@ export const table: CustomTheme = {
   radii: { 0: '4px', composer: '8px' },
   fontSize: { 0: '12px', 1: '14px', 2: '14px', 3: '32px', button: '12px' },
   lineHeights: { 0: '18.36px', 1: '22.4px' },
-  fontWeights: { 2: 500, 3: 700, button: 600 },
+  fontWeights: {
+    2: 500,
+    3: 700,
+    button: 600,
+    mention: 600,
+    mentionDropdownItemMark: 600,
+    profileNameText: 600,
+  },
   sizes: {
     avatar: '32px',
   },
@@ -29,6 +36,9 @@ export const table: CustomTheme = {
     buttonSecondaryHover: '',
     buttonTertiaryHover: '',
   },
+  space: {
+    commentHeaderBodyGap: '12px',
+  },
   shadows: {
     mentionDropdownBoxShadow:
       '0px -12px 24px rgba(0, 0, 0, 0.02), 0px 12px 24px rgba(0, 0, 0, 0.06)',
@@ -39,9 +49,19 @@ export const table: CustomTheme = {
     sendButtonTextColor: 'white',
     sendButtonDisabledTextColor: '#B4BDC2',
     buttonPrimaryBackground: '#36B374',
+
+    mentionText: '#007FF5',
+    mentionDropdownMarkBackground: 'none',
+    mentionDropdownTextColor: 'black',
+
     buttonPrimaryText: 'white',
-    buttonPrimaryHoverBackground: '#36B374',
+    buttonPrimaryHoverBackground:
+      'linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), #36B374',
     buttonPrimaryHoverText: 'white',
+
+    buttonPrimaryActiveBackground:
+      'linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #36B374',
+    buttonPrimaryActiveText: 'white',
 
     buttonSecondaryBackground: 'transparent',
     buttonSecondaryText: '#6A7278',
@@ -49,7 +69,11 @@ export const table: CustomTheme = {
     buttonTertiaryBackground: 'white',
     buttonTertiaryText: '#6A7278',
 
+    buttonTertiaryHoverBackground: 'rgba(0, 0, 0, 0.05)',
+    buttonTertiaryActiveBackground: 'rgba(0, 0, 0, 0.1)',
+
     buttonDisabledText: '#B4BDC2',
+    buttonDisabledBackground: '#E3E9ED',
 
     backgroundColor: 'white',
     composerBackground: 'white',
