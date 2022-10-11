7447d5e8365f83a635df9ff88b0cd3547d6b4a12
Namit Chadha <namit.chadha@gmail.com>
2022-10-11 16:14:44 -0700

    use popover thread docs complete

--- www/src/docs/hooks/usePopoverThreadDoc.tsx
+++ www/src/docs/hooks/usePopoverThreadDoc.tsx
@@ -4,23 +4,24 @@ import { Markdown } from '../Markdown';
 export function UsePopoverThreadDoc() {
   return (
     <>
-      <h2>Open or close a Popover Thread.</h2>
+      <h2>Control a Popover Thread.</h2>
       <div>
         <h3>Usage</h3>
         <p>
-          <code>usePopoverThread</code> lets you control a popover thread. It returns{' '}
-          <code>setPopoverState</code> which can be used to <code>open</code> or <code>close</code>{' '}
-          a thread or show its <code>preview</code>.
+          <code>usePopoverThread</code> lets you open, close or show the preview by calling the{' '}
+          <code>setPopoverState</code> function it returns.
         </p>
         <p>
           Must be called within a <code>CollabKit.Provider</code>
         </p>
+
         {renderCodeSnippet(`import { usePopoverThread, PopoverTrigger } from '@collabkit/react';
 
 export function App() {
   const { context, setPopoverState } = usePopoverThread({ threadId: 'unique-thread-id' });
 
   return <div>
+    <button onClick={() => setPopoverState('open')}>Open Sesame</button>
     <PopoverTrigger context={context}>
       Hello world
     </PopoverTrigger>
@@ -36,6 +37,18 @@ export function App() {
 #### threadId: string
 
 The thread ID of the Popover Thread.`}
+      />
+
+      <Markdown
+        body={`### Returns
+
+#### setPopoverState: (state: 'open' | 'close' | 'preview') => void
+
+Sets the popover state.
+
+#### context: PopoverContext
+
+The Popover Context. Pass this to \`PopoverTrigger\` to anchor the Popover Thread to a component.`}
       />
     </>
   );
