a63cade718a28a328f2302f0178023d1ed4ed21c
Ville Immonen <ville.immonen@iki.fi>
2022-10-12 21:23:47 +0300

    Add data grid to /datagrid

--- www/src/pages/DataGridPage.tsx
+++ www/src/pages/DataGridPage.tsx
@@ -1,7 +1,18 @@
+import { DataGridDemo } from '../components/DataGridDemo';
+
 export function DataGridPage() {
   return (
-    <div>
-      <h1>DataGrid Landing Page</h1>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        background: '#FFEE78',
+        paddingBlock: '140px',
+        paddingInline: '16px',
+        boxSizing: 'border-box',
+      }}
+    >
+      <DataGridDemo />
     </div>
   );
 }
