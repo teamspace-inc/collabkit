c1b08dec6b219a827852a93eb708c858884a64c9
Namit Chadha <namit.chadha@gmail.com>
2022-10-15 19:34:51 -0700

    fix CK-179 inbox hover state fix

--- packages/@collabkit/react/src/components/InboxButton.tsx
+++ packages/@collabkit/react/src/components/InboxButton.tsx
@@ -3,7 +3,6 @@ import { useInboxButton } from '../hooks/public/useInboxButton';
 import { useUnreadThreadsCount } from '../hooks/public/useUnreadThreadsCount';
 import { inboxButton } from '../styles/components/InboxButton.css';
 
-// Cashboard icons
 import CommentIcon from './Comment.svg';
 import CommentNotificationIcon from './CommentNotification.svg';
 import { ThemeWrapper } from './ThemeWrapper';
@@ -15,29 +14,7 @@ export function InboxButton() {
 
   return (
     <ThemeWrapper>
-      {/* Cashboard specific styles */}
-      <button
-        className={inboxButton}
-        // style={{
-        //   fontSize: '14px',
-        //   border: '1px solid #B4BDC2',
-        //   borderRadius: '6px',
-        //   fontWeight: 600,
-        //   lineHeight: '160%',
-        //   letterSpacing: -0.1,
-        //   color: '#6A7278',
-        //   background: 'transparent',
-        //   padding: '8px 16px',
-        //   boxSizing: 'border-box',
-        //   height: 40,
-        //   display: 'flex',
-        //   gap: '8px',
-        //   alignItems: 'center',
-        //   justifyContent: 'center',
-        //   cursor: 'pointer',
-        // }}
-        onPointerDown={onPointerDown}
-      >
+      <button className={inboxButton} onPointerDown={onPointerDown}>
         {showUnreadDot ? <img src={CommentNotificationIcon} /> : <img src={CommentIcon} />}
         All comments
       </button>
--- packages/@collabkit/react/src/styles/components/InboxButton.css.ts
+++ packages/@collabkit/react/src/styles/components/InboxButton.css.ts
@@ -1,26 +1,9 @@
 import { style } from '@vanilla-extract/css';
-// import { vars } from '../theme';
 import { button } from './Button.css';
 
 export const inboxButton = style([
   button({ type: 'primary' }),
   {
     gap: '8px',
-    // fontSize: '14px',
-    // border: `1px solid ${vars.color.border}`,
-    // borderRadius: '6px',
-    // fontWeight: 600,
-    // lineHeight: '160%',
-    // letterSpacing: -0.1,
-    // color: '#6A7278',
-    // background: vars.button.primary.background,
-    // padding: '8px 16px',
-    // boxSizing: 'border-box',
-    // height: 40,
-    // display: 'flex',
-    // gap: '8px',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    // cursor: 'pointer',
   },
 ]);
