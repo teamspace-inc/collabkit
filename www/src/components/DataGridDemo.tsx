ff4c52fd8d61fed7967d22df41c47a01e4ee26c6
Ville Immonen <ville.immonen@iki.fi>
2022-10-17 16:07:10 +0300

    Table cell width

--- www/src/components/DataGridDemo.tsx
+++ www/src/components/DataGridDemo.tsx
@@ -139,9 +139,9 @@ export function DataGridDemo() {
   ]);
 
   const [columnDefs] = useState<AgGridReactProps['columnDefs']>([
-    { headerName: '', field: 'account', cellRenderer: CellRenderer, cellStyle },
-    { headerName: 'Budget', field: 'budget', cellRenderer: CellRenderer, cellStyle },
-    { headerName: 'Budget Spent', field: 'spent', cellRenderer: CellRenderer, cellStyle },
+    { headerName: '', field: 'account', cellRenderer: CellRenderer, cellStyle, flex: 1.5 },
+    { headerName: 'Budget', field: 'budget', cellRenderer: CellRenderer, cellStyle, flex: 1.5 },
+    { headerName: 'Budget Spent', field: 'spent', cellRenderer: CellRenderer, cellStyle, flex: 1 },
     { headerName: 'Change', field: 'change', cellRenderer: CellRenderer, cellStyle, flex: 1 },
   ]);
 
