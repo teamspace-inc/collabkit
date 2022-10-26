f92947e3c7a738c0a05a69df56d8633a95e77b1b
Ville Immonen <ville.immonen@iki.fi>
2022-10-26 18:58:14 +0300

    Update ListDemo styles

--- www/src/home/ListDemo.css.ts
+++ www/src/home/ListDemo.css.ts
@@ -32,3 +32,83 @@ export const role = style({
 });
 
 export const checkbox = style({});
+
+export const modal = style({
+  position: 'absolute',
+  inset: 0,
+});
+
+export const sidebar = style({
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  bottom: 0,
+
+  borderLeft: '1px solid #E1E7ED',
+
+  display: 'flex  ',
+  flexDirection: 'column',
+  backgroundColor: 'white',
+  padding: 16,
+});
+
+export const sidebarTitle = style({
+  fontStyle: 'normal',
+  fontWeight: '600',
+  fontSize: '14px',
+  lineHeight: '95%',
+  color: vars.color.textContrastHigh,
+  marginBottom: 40,
+});
+
+export const avatar = style({
+  background: vars.color.yellow,
+  borderRadius: '50%',
+  alignSelf: 'center',
+  width: 120,
+  height: 120,
+  marginBottom: 20,
+});
+
+export const employeeName = style({
+  fontStyle: 'normal',
+  fontWeight: '600',
+  fontSize: '24px',
+  lineHeight: '23px',
+  letterSpacing: 0,
+  textAlign: 'center',
+  color: vars.color.textContrastHigh,
+
+  marginBottom: 12,
+});
+
+export const employeeRole = style({
+  fontStyle: 'normal',
+  fontWeight: '400',
+  fontSize: '14px',
+  lineHeight: '95%',
+  textAlign: 'center',
+  color: vars.color.textContrastLow,
+
+  marginBottom: 20,
+});
+
+export const detailsBox = style({
+  fontStyle: 'normal',
+  fontWeight: '400',
+  fontSize: '12px',
+  lineHeight: '140%',
+  color: vars.color.textContrastHigh,
+
+  border: '1px solid #E1E7ED',
+  borderRadius: 8,
+  padding: 16,
+
+  display: 'flex',
+  flexWrap: 'wrap',
+});
+
+export const fieldName = style({
+  fontWeight: '700',
+  width: '50%',
+});
--- www/src/home/ListDemo.tsx
+++ www/src/home/ListDemo.tsx
@@ -1,8 +1,18 @@
 import { useState } from 'react';
-import { Inbox, Sidebar, ThemeProvider } from '@collabkit/react';
+import { Inbox, ThemeProvider, Thread } from '@collabkit/react';
 import { light } from '../styles/Theme.css';
 import { AcmeLogo, Container, Heading, HeadingRow, UI } from './DemoUI';
 import * as styles from './ListDemo.css';
+
+import avatar1 from '../assets/home/list/employees/5e6802566d3b380006d3f13b_transparent.png';
+import avatar2 from '../assets/home/list/employees/5e685b006d3b380006e7ad45_transparent.png';
+import avatar3 from '../assets/home/list/employees/5e6869806d3b380006eaf699_transparent.png';
+import avatar4 from '../assets/home/list/employees/5e6876c86d3b380006edf213_transparent.png';
+import avatar5 from '../assets/home/list/employees/5e687bc56d3b380006ef19fb_transparent.png';
+import avatar6 from '../assets/home/list/employees/5e6886b66d3b380006f19c8f_transparent.png';
+import avatar7 from '../assets/home/list/employees/5e6887c36d3b380006f1da63_transparent.png';
+import avatar8 from '../assets/home/list/employees/5e6888a66d3b380006f20e33_transparent.png';
+import avatar9 from '../assets/home/list/employees/5e6888f06d3b380006f21e9d_transparent.png';
 
 type Employee = {
   id: string;
@@ -10,6 +20,7 @@ type Employee = {
   role: string;
   hiredOn: string;
   department: string;
+  avatar: string;
 };
 
 const employees: Employee[] = [
@@ -19,6 +30,7 @@ const employees: Employee[] = [
     role: 'Marketing Coordinator',
     hiredOn: '04/09/22',
     department: 'Sales',
+    avatar: avatar1,
   },
   {
     id: '8195063A',
@@ -26,6 +38,7 @@ const employees: Employee[] = [
     role: 'Product Designer',
     hiredOn: '04/09/22',
     department: 'R&D',
+    avatar: avatar2,
   },
   {
     id: '1766698L',
@@ -33,6 +46,7 @@ const employees: Employee[] = [
     role: 'Product Designer',
     hiredOn: '04/09/22',
     department: 'Design',
+    avatar: avatar3,
   },
   {
     id: '2608510R',
@@ -40,6 +54,7 @@ const employees: Employee[] = [
     role: 'Senior Engineer',
     hiredOn: '04/09/22',
     department: 'Infrastructure',
+    avatar: avatar4,
   },
   {
     id: '4991791M',
@@ -47,6 +62,7 @@ const employees: Employee[] = [
     role: 'Engineer',
     hiredOn: '04/09/22',
     department: 'Research',
+    avatar: avatar5,
   },
   {
     id: '3374135A',
@@ -54,6 +70,7 @@ const employees: Employee[] = [
     role: 'Engineer',
     hiredOn: '04/09/22',
     department: 'Engineering',
+    avatar: avatar6,
   },
   {
     id: '9586331U',
@@ -61,6 +78,7 @@ const employees: Employee[] = [
     role: 'Account Executive',
     hiredOn: '04/09/22',
     department: 'Advertising',
+    avatar: avatar7,
   },
   {
     id: '3600818H',
@@ -68,6 +86,7 @@ const employees: Employee[] = [
     role: 'Project Manager',
     hiredOn: '04/09/22',
     department: 'Engineering',
+    avatar: avatar8,
   },
   {
     id: '8789935W',
@@ -75,6 +94,7 @@ const employees: Employee[] = [
     role: 'Senior Engineer',
     hiredOn: '04/10/17',
     department: 'Engineering',
+    avatar: avatar9,
   },
 ];
 
@@ -90,10 +110,35 @@ function Row({ employee, onClick }: { employee: Employee; onClick: (id: string) 
   );
 }
 
-export function ListDemo() {
-  const [selectedId, setSelectedId] = useState(employees[0].id);
+function Sidebar({ employee }: { employee: Employee | undefined }) {
+  if (employee == null) return null;
+  console.log('thread');
   return (
-    <ThemeProvider theme="dark">
+    <div className={styles.modal}>
+      <div className={styles.sidebar}>
+        <div className={styles.sidebarTitle}>Details</div>
+        <img className={styles.avatar} src={employee.avatar} />
+        <div className={styles.employeeName}>{employee.name}</div>
+        <div className={styles.employeeRole}>{employee.role}</div>
+        <dl className={styles.detailsBox}>
+          <dt className={styles.fieldName}>ID</dt>
+          <dd>{employee.id}</dd>
+          <dt className={styles.fieldName}>Hiring date</dt>
+          <dd>{employee.hiredOn}</dd>
+          <dt className={styles.fieldName}>Department</dt>
+          <dd>{employee.department}</dd>
+        </dl>
+        <Thread threadId={`employee-${employee.id}`} />
+      </div>
+    </div>
+  );
+}
+
+export function ListDemo() {
+  const [selectedId, setSelectedId] = useState(employees[3].id);
+  const selectedEmployee = selectedId ? employees.find(({ id }) => id === selectedId) : undefined;
+  return (
+    <ThemeProvider theme="light">
       <div className={light} style={{ display: 'contents' }}>
         <UI>
           <div>
@@ -109,9 +154,7 @@ export function ListDemo() {
               ))}
             </div>
           </Container>
-          <Sidebar strategy="absolute">
-            <Inbox />
-          </Sidebar>
+          <Sidebar employee={selectedEmployee} />
         </UI>
       </div>
     </ThemeProvider>
