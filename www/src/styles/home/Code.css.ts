f093b60d06a134d7ca18c4529b58c0e339d4c7d8
Namit Chadha <namit.chadha@gmail.com>
2022-10-28 13:02:18 +0100

    code

--- www/src/styles/home/Code.css.ts
+++ www/src/styles/home/Code.css.ts
@@ -3,3 +3,17 @@ import { style } from '@vanilla-extract/css';
 export const h3 = style({
   color: '#ADA0C0 !important',
 });
+
+export const codeWrap = style({
+  display: 'flex',
+  padding: '120px 0 120px',
+  background: 'rgba(0, 0, 0, 0.3)',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  marginBottom: '-160px',
+});
+
+export const section = style({
+  paddingTop: '40px !important',
+});
