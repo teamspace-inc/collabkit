061927e80952098b0e368b718442414cf3fc29f9
Namit Chadha <namit.chadha@gmail.com>
2022-10-30 14:30:22 +0000

    thread profiles now returns mentions too

--- functions/src/actions/helpers/getThreadProfiles.tsx
+++ functions/src/actions/helpers/getThreadProfiles.tsx
@@ -2,5 +2,10 @@ import { Timeline } from '../../types';
 import uniq from 'lodash.uniq';
 
 export function getThreadProfiles(props: { timeline: Timeline }) {
-  return uniq(Object.values(props.timeline).map((event) => event.createdById));
+  const createdByIds = uniq(Object.values(props.timeline).map((event) => event.createdById));
+  const mentionedIds = Object.values(props.timeline)
+    .map((event) => Object.keys(event.mentions ?? {}))
+    .flat(1);
+  const profileIds = uniq([...createdByIds, ...mentionedIds]);
+  return profileIds;
 }
