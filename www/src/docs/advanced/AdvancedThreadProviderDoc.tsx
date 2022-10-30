6a6cff1c67581f6397dda5f0b580b699fb078bcf
Namit Chadha <namit.chadha@gmail.com>
2022-10-30 12:17:23 +0000

    style

--- www/src/docs/advanced/AdvancedThreadProviderDoc.tsx
+++ www/src/docs/advanced/AdvancedThreadProviderDoc.tsx
@@ -4,7 +4,7 @@ import { DocLink } from '../Doc';
 export function AdvancedThreadProviderDoc() {
   return (
     <>
-      <h2>Provides thread context for other components.</h2>
+      <h2>Provides thread context for other components</h2>
       <blockquote>
         <h4 style={{ marginTop: 0 }}>Advanced</h4>
         This doc covers how to set up context for rendering individual Comment or Profile
