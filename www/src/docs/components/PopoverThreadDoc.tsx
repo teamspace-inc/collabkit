62f0ef826b05d66b27a50c031c4393a449a167d2
Namit Chadha <namit.chadha@gmail.com>
2022-10-31 09:26:14 +0000

    popover thread docs

--- www/src/docs/components/PopoverThreadDoc.tsx
+++ www/src/docs/components/PopoverThreadDoc.tsx
@@ -1,4 +1,5 @@
 import { renderCodeSnippet } from '../CodeEditor';
+import { DocLink } from '../Doc';
 import { PopoverThreadDemo } from './PopoverThreadDemo';
 
 export function PopoverThreadDoc() {
@@ -36,6 +37,47 @@ export function App() {
     </PopoverTrigger>
   </div>
 }`)}
+        <p>
+          Wrap the component you wish to anchor the PopoverThread to with a PopoverTrigger. And
+          configure its <code>context</code> using <code>usePopoverThread</code>.{' '}
+        </p>
+      </div>
+      <div>
+        <h2>usePopoverThread</h2>
+        <br />
+        <h3>Props</h3>
+        <h4>threadId: string</h4>
+        <p>A unique ID that represents this thread.</p>
+        <h4>name?: string</h4>
+        <p>
+          A name for the thread. Used in email notifications to refer to the thread. e.g. if you set
+          name to ‘Task 1’ and ‘John’ writes a comment, the email subject will be ‘John left a
+          comment on Task 1’
+        </p>
+        <h4>url?: string</h4>
+        <p>
+          The URL of the webpage this thread is rendered on. Used in email notifications and the
+          <DocLink href="/docs/inbox">Inbox</DocLink> to help a user get back to a thread. <br />
+          Defaults to <code>window.location.href</code>
+        </p>
+        <h4>autoFocus?: boolean</h4>
+        <p>
+          Set this to automatically focus the composer input on render. <br />
+          Defaults to <code>true</code>
+        </p>
+        <h4>hideComposer?: boolean</h4>
+        <p>
+          Hide the composer. <br />
+          Defaults to <code>false</code>
+        </p>
+
+        <h2>PopoverTrigger</h2>
+        <br />
+        <h3>Props</h3>
+        <h4>Context: {`ReturnType<typeof usePopoverThread>['context']`}</h4>
+        <p>
+          Context from <code>usePopoverThread</code> to pass to <code>PopoverTrigger</code>
+        </p>
       </div>
     </>
   );
