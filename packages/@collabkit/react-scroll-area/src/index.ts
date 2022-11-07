663fb724902918279c964aaa89019b6a5596bc95
Ville Immonen <ville.immonen@iki.fi>
2022-11-07 16:52:18 +0200

    Add a fork of @radix-ui/react-scroll-area

---
+++ packages/@collabkit/react-scroll-area/src/index.ts
@@ -0,0 +1,22 @@
+export {
+  createScrollAreaScope,
+  //
+  ScrollArea,
+  ScrollAreaViewport,
+  ScrollAreaScrollbar,
+  ScrollAreaThumb,
+  ScrollAreaCorner,
+  //
+  Root,
+  Viewport,
+  Scrollbar,
+  Thumb,
+  Corner,
+} from './ScrollArea';
+export type {
+  ScrollAreaProps,
+  ScrollAreaViewportProps,
+  ScrollAreaScrollbarProps,
+  ScrollAreaThumbProps,
+  ScrollAreaCornerProps,
+} from './ScrollArea';
