4ef47984cad22e1afd2bd9bd9df68b1b91aa68b1
Namit Chadha <namit.chadha@gmail.com>
2022-11-09 03:02:51 +0000

    header links from docs work correctly

--- www/src/home/Header.tsx
+++ www/src/home/Header.tsx
@@ -6,6 +6,7 @@ import { content, rightLinks, root } from '../styles/Header.css';
 import { proxy, useSnapshot } from 'valtio';
 import { dark, light, vars } from '../styles/Theme.css';
 import { useLocation } from 'wouter';
+import { useCallback } from 'react';
 
 export const store = proxy<{
   backgroundColor: string;
@@ -31,14 +32,36 @@ export function StickyHeader(props: {
   );
 }
 
+export function useScrollToLink(props: { selector: string }) {
+  const [, setLocation] = useLocation();
+  return useCallback(
+    (opts?: { behaviour?: 'smooth' | 'auto' }) => {
+      const element = document.querySelector(props.selector);
+      if (element) {
+        element.scrollIntoView({ behavior: opts?.behaviour ?? 'smooth', block: 'start' });
+        setLocation(props.selector);
+      }
+    },
+    [props.selector]
+  );
+}
+
 function ScrollToLink(props: { selector: string; children: React.ReactNode; href: string }) {
-  const [location, setLocation] = useLocation();
+  const scrollToLink = useScrollToLink(props);
+  const [, setLocation] = useLocation();
   return window.innerWidth > 640 ? (
     <Link
       className={a}
       onClick={() => {
-        const el = document.querySelectorAll(props.selector)[0];
-        el ? el.scrollIntoView({ behavior: 'smooth', block: 'start' }) : setLocation(props.href);
+        if (window.location.href.toString().includes('/docs')) {
+          setLocation('/');
+          setTimeout(() => {
+            scrollToLink({ behaviour: 'auto' });
+          }, 1);
+          scrollToLink();
+        } else {
+          scrollToLink();
+        }
       }}
     >
       {props.children}
@@ -47,7 +70,7 @@ function ScrollToLink(props: { selector: string; children: React.ReactNode; href
 }
 
 function HeaderRightLinks() {
-  const [location, setLocation] = useLocation();
+  const [, setLocation] = useLocation();
 
   return (
     <div className={rightLinks}>
@@ -69,6 +92,7 @@ function HeaderRightLinks() {
 
 export function Header() {
   const { backgroundColor, theme } = useSnapshot(store);
+  const [location, setLocation] = useLocation();
 
   return (
     <StickyHeader
@@ -77,11 +101,15 @@ export function Header() {
         <Logo
           theme={theme}
           onClick={() => {
-            window.scrollTo({
-              top: 0,
-              left: 0,
-              behavior: 'smooth',
-            });
+            if (window.location.href.toString().includes('/docs')) {
+              setLocation('/');
+            } else {
+              window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth',
+              });
+            }
           }}
         />
       }
