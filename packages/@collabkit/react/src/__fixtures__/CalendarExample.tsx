c33b6f75003a76a3d4b85e13958fd98d6bfda4b0
Ville Immonen <ville.immonen@iki.fi>
2022-11-23 17:37:51 +0200

    Remove unused __fixtures__

--- packages/@collabkit/react/src/__fixtures__/CalendarExample.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Calendar from 'react-calendar';
-
-import 'react-calendar/dist/Calendar.css';
-
-export default function CalendarExample() {
-  return <Calendar showWeekNumbers />;
-}
