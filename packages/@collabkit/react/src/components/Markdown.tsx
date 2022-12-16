e444b692f4bc1159f3fdfe6eabbc61339947f23b
Namit Chadha <namit.chadha@gmail.com>
2022-12-16 12:57:43 +0000

    improve markdown rendering

--- packages/@collabkit/react/src/components/Markdown.tsx
+++ packages/@collabkit/react/src/components/Markdown.tsx
@@ -1,20 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import { useRemark } from 'react-remark';
+import React from 'react';
+import { useRemarkSync } from 'react-remark';
 
 export const Markdown = React.memo(function Markdown(props: {
   className?: string;
   body: string;
   onLinkClick?: (e: React.MouseEvent) => void;
 }) {
-  const mountedRef = useRef(false);
-  const [reactContent, setMarkdownSource] = useRemark();
-
-  // prevent flickr on initial render
-  !mountedRef.current && setMarkdownSource(props.body);
-  useEffect(() => {
-    mountedRef.current = true;
-    setMarkdownSource(props.body);
-  }, [props.body]);
-
+  const reactContent = useRemarkSync(props.body);
   return <div className={props.className}>{reactContent}</div>;
 });
