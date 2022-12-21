95d8ea251e78dc884fdf0a4c9fd6c342dce95d7a
Namit Chadha <namit.chadha@gmail.com>
2022-12-21 20:41:42 +0000

    reduce words

--- www/src/docs/IntroductionDoc.tsx
+++ www/src/docs/IntroductionDoc.tsx
@@ -6,12 +6,8 @@ export function IntroductionDoc() {
   return (
     <>
       <h2>
-        CollabKit is a drop-in React &amp; Vue SDK for adding commenting to your app. It's fully
-        customisable, can be integrated in minutes.
-        <br />
-        <br />
-        Add our components to your app to help your users collaborate in context, and work as a
-        team.
+        Add collaboration to your app with our customisable React SDK. In minutes your users will be
+        able to work as a team within your app.
       </h2>
       <DocFooterLink
         path={['Getting Started']}
@@ -21,20 +17,11 @@ export function IntroductionDoc() {
       <div>
         <h3>Vision</h3>
         <p>
-          Most SaaS apps need some form of collaboration. It’s the underlying activity we perform
-          while working with others in a team, and the tools we use have the context about the work
-          we’re doing.
-        </p>
-        <p>
-          Purpose built collaboration apps like Notion, Figma and Google Docs have offered
-          commenting for many years now, but for the average app, building one is usually a
-          significant undertaking, or one done to a bare-minimum standard which misses users
-          expectations set by industry leading products.
-        </p>
-        <p>
-          Our goal is to create a high-quality, easy-to-integrate commenting system, which helps
-          your users collaborate effectively, in your product, with full context. So you can focus
-          on your core value and meet your users expectations with minimal effort.
+          Collaboration is a vital part of most SaaS apps, but building a high-quality commenting
+          system can be a major undertaking. That's where we come in. Our goal is to create a
+          top-notch, easy-to-integrate commenting system that helps your users collaborate
+          effectively within your product. That way, you can focus on your core value and meet your
+          users' expectations with minimal effort.
         </p>
       </div>
       <div>
@@ -43,33 +30,24 @@ export function IntroductionDoc() {
           <li>
             <p>
               <b>Contextual Collaboration: </b>
-              Comment on anything in your app, whether be it a table, chart, piece of text, or a
-              sale, invoice, or product. CollabKit works with any interface, letting your users
-              leave comments on the item in question.
+              Comment on anything in your app.
             </p>
           </li>
           <li>
             <p>
-              <b>Easy to integrate: </b> Drop-in our SDK and ship commenting in minutes. We take
-              care of all the hard bits like realtime, seen state, email notifications, rich text
-              formatting, emojis and more…
+              <b>Easy to integrate: </b> Drop-in our SDK and ship collaboration in minutes.
             </p>
           </li>
           <li>
             <p>
               <b>Customisable: </b>
-              Fully customisable, pick the components that suit your apps interface and adjust the
-              theme to set font size, line heights, spacing, padding, margins, typography and
-              colours. Match your apps look and feel so it looks like you built it in-house. Or go
-              all the way and use our unstyled building blocks to create an entirely new commenting
-              or collaboration experience completely integrated with your app.
+              Match your app's look and feel
             </p>
           </li>
           <li>
             <p>
               <b>Human: </b>
-              Let your users work as a team in your product. With emojis, GIFs, file and photo
-              sharing, and replies your users will feel right at home with our commenting system.
+              Emojis, GIFs, file sharing, and replies make collaboration feel like home
             </p>
           </li>
         </ol>
