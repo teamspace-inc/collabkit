6c0f852defc89b4df31727352d9bfebaad0ebc2b
Namit Chadha <namit.chadha@gmail.com>
2023-01-09 22:53:27 +0000

    custom markdown parser

--- packages/@collabkit/react/src/components/Markdown.tsx
+++ packages/@collabkit/react/src/components/Markdown.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import { useRemarkSync } from 'react-remark';
+import React, { createElement, Fragment, useEffect, useState } from 'react';
+import rehypeReact from 'rehype-react';
 import remarkRehype from 'remark-rehype';
 import remarkParse from 'remark-parse';
+import { unified } from 'unified';
+
+function MarkdownLink(props: any) {
+  return <a {...props} />;
+}
+
+function useMarkdown(md: string) {
+  const [reactContent, setReactContent] = useState<React.ReactNode>(null);
+  useEffect(() => {
+    unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeReact, {
+        createElement,
+        Fragment,
+        components: {
+          a: MarkdownLink,
+        },
+      })
+      .process(md)
+      .then((file) => {
+        setReactContent(file.result as React.ReactNode);
+      });
+  }, [md]);
+  return reactContent;
+}
 
 export const Markdown = React.memo(function Markdown(props: { className?: string; body: string }) {
-  const reactContent = useRemarkSync(props.body);
+  const reactContent = useMarkdown(props.body);
   return <div className={props.className}>{reactContent}</div>;
 });
--- www/src/docs/Markdown.tsx
+++ www/src/docs/Markdown.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { useRemarkSync } from 'react-remark';
+import { remark } from 'remark';
+import rehypeReact from 'rehype-react';
+
+function useMarkdown(md: string) {
+  console.log(remark().processSync(md));
+}
 
 export const Markdown = React.memo(function Markdown(props: { className?: string; body: string }) {
+  useMarkdown(props.body);
   const reactContent = useRemarkSync(props.body);
   return <div className={props.className}>{reactContent}</div>;
 });
