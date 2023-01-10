17bb0e60406129b1bd584af8bc81db24bad7a58b
Namit Chadha <namit.chadha@gmail.com>
2023-01-10 12:12:29 +0000

    use stock chrome

--- packages/@collabkit/react/playwright.config.ts
+++ packages/@collabkit/react/playwright.config.ts
@@ -40,59 +40,60 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    channel: 'chrome',
   },
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
+  // /* Configure projects for major browsers */
+  // projects: [
+  //   {
+  //     name: 'chromium',
+  //     use: {
+  //       ...devices['Desktop Chrome'],
+  //     },
+  //   },
 
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   },
-    // },
+  //   // {
+  //   //   name: 'firefox',
+  //   //   use: {
+  //   //     ...devices['Desktop Firefox'],
+  //   //   },
+  //   // },
 
-    // {
-    //   name: 'webkit',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   },
-    // },
+  //   // {
+  //   //   name: 'webkit',
+  //   //   use: {
+  //   //     ...devices['Desktop Safari'],
+  //   //   },
+  //   // },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
+  //   /* Test against mobile viewports. */
+  //   // {
+  //   //   name: 'Mobile Chrome',
+  //   //   use: {
+  //   //     ...devices['Pixel 5'],
+  //   //   },
+  //   // },
+  //   // {
+  //   //   name: 'Mobile Safari',
+  //   //   use: {
+  //   //     ...devices['iPhone 12'],
+  //   //   },
+  //   // },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
-  ],
+  //   /* Test against branded browsers. */
+  //   // {
+  //   //   name: 'Microsoft Edge',
+  //   //   use: {
+  //   //     channel: 'msedge',
+  //   //   },
+  //   // },
+  //   // {
+  //   //   name: 'Google Chrome',
+  //   //   use: {
+  //   //     channel: 'chrome',
+  //   //   },
+  //   // },
+  // ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
