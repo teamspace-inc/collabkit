35f86a4d6d7913672fcdbb16f392b61219e7166c
Namit Chadha <namit.chadha@gmail.com>
2023-01-24 12:06:49 +0000

    pending pins are now populated when editing a message

--- packages/@collabkit/client/src/actions/initComposer.ts
+++ packages/@collabkit/client/src/actions/initComposer.ts
@@ -8,6 +8,18 @@ export function initComposer(
   const { workspaceId, threadId, eventId } = props;
   const composers = store.workspaces[workspaceId].composers;
   composers[threadId] ??= {};
+
+  if (!composers[threadId][eventId]) {
+    composers[threadId][eventId] = createComposer();
+
+    if (store.workspaces[workspaceId].eventPins[eventId]) {
+      composers[threadId][eventId].pendingPin = {
+        ...store.workspaces[workspaceId].eventPins[eventId],
+        isPending: true,
+      };
+    }
+  }
+
   composers[threadId][eventId] ??= createComposer();
   return composers[threadId][eventId];
 }
