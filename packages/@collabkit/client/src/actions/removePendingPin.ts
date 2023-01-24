06722d3107f3e0af169394f81a5b2e3182c238dd
Namit Chadha <namit.chadha@gmail.com>
2023-01-24 11:35:19 +0000

    remove console.log

--- packages/@collabkit/client/src/actions/removePendingPin.ts
+++ packages/@collabkit/client/src/actions/removePendingPin.ts
@@ -4,7 +4,6 @@ export function removePendingPin(
   store: Store,
   props: { workspaceId: string; eventId: string; threadId: string }
 ) {
-  console.log('removePendingPin', props);
   store.workspaces[props.workspaceId].composers[props.threadId][props.eventId].pendingPin = null;
   store.uiState = 'idle';
 }
