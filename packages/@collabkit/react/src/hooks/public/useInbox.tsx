a6d6f785a2e8d9881331943a544a33203dc895aa
Ville Immonen <ville.immonen@iki.fi>
2023-01-30 17:09:24 +0200

    Add threadIds prop to Inbox (#156)
    * Add threadIds prop to Inbox
    
    * Temporarily use ubuntu-latest instead of ubuntu-latest-16-cores
    
    https://www.githubstatus.com/incidents/d1l8lxw26y8k

--- packages/@collabkit/react/src/hooks/public/useInbox.tsx
+++ packages/@collabkit/react/src/hooks/public/useInbox.tsx
@@ -4,7 +4,7 @@ import { useSnapshot } from 'valtio';
 import { useApp } from '../../hooks/useApp';
 import { timelineUtils } from '@collabkit/core';
 
-export function useInbox(props: { filter: 'all' | 'open' }) {
+export function useInbox(props: { filter: 'all' | 'open'; threadIds?: string[] }) {
   const { store } = useApp();
   const { workspaceId, workspaces, userId } = useSnapshot(store);
 
@@ -30,26 +30,26 @@ export function useInbox(props: { filter: 'all' | 'open' }) {
 
   // todo this won't scale so we should add a view to load
   // inboxResolved and inboxOpen
-  const threadIds = inbox
-    ? // show threads with latest activity first
-    Object.keys(inbox)
+
+  const threadIds = props.threadIds ?? Object.keys(inbox);
+  return (
+    threadIds
       // filter out resolved threads
+      .filter(
+        (threadId) =>
+          threadId &&
+          (props.filter === 'open'
+            ? !(
+                workspace.timeline?.[threadId] &&
+                timelineUtils.computeIsResolved(workspace.timeline?.[threadId])
+              )
+            : true)
+      )
+      // show threads with latest activity first
       .sort((a, b) => {
-        const aTime = +inbox[a].createdAt ?? 0;
-        const bTime = +inbox[b].createdAt ?? 0;
+        const aTime = +inbox[a]?.createdAt ?? 0;
+        const bTime = +inbox[b]?.createdAt ?? 0;
         return bTime - aTime;
       })
-      // filter out resolved threads
-      ?.filter((threadId) =>
-        props.filter === 'open'
-          ? !(
-              workspace.timeline?.[threadId] &&
-              timelineUtils.computeIsResolved(workspace.timeline?.[threadId])
-            )
-          : true
-      )
-      .filter(Boolean)
-    : [];
-
-  return threadIds;
+  );
 }
