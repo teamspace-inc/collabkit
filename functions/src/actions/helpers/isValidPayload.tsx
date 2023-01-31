fbf88fd7d0ab0edd89e60d115d09933309ff878e
Meet Shah <meetcshah19@gmail.com>
2023-01-31 23:14:33 +0530

    fix tests

--- functions/src/actions/helpers/isValidPayload.tsx
+++ functions/src/actions/helpers/isValidPayload.tsx
@@ -9,7 +9,7 @@ export function isValidPayload(o: unknown): o is Payload {
     if (typeof o !== 'object' || o == null || Array.isArray(o)) {
         return false;
       }
-    const userIdValid = !has(o, 'userId') || o.userId === null || typeof o.userId === 'string';
-    const workspaceIdValid = !has(o, 'workspaceId') || o.workspaceId === null || typeof o.workspaceId === 'string';
+    const userIdValid = !has(o, 'userId') || typeof o.userId === 'string';
+    const workspaceIdValid = !has(o, 'workspaceId') || typeof o.workspaceId === 'string';
     return userIdValid && workspaceIdValid;
 }
