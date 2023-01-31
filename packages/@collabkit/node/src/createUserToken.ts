9e5852968793c61997ba4671818d0062661f249a
Meet Shah <meetcshah19@gmail.com>
2023-01-31 22:55:26 +0530

    add expiry

--- packages/@collabkit/node/src/createUserToken.ts
+++ packages/@collabkit/node/src/createUserToken.ts
@@ -1,5 +1,5 @@
 import * as jwt from 'jsonwebtoken';
 
 export function createUserToken(apiKey: string, userId: string, workspaceId: string) {
-  return jwt.sign({ userId: userId, workspaceId: workspaceId }, apiKey);
+  return jwt.sign({ userId: userId, workspaceId: workspaceId }, apiKey, { expiresIn : "1h" });
 }
