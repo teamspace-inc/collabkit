257d7d2ecac1d2e8d833bb11072566db9079a4f0
Meet Shah <meetcshah19@gmail.com>
2023-02-01 18:40:47 +0530

    fix formatting and host

--- packages/@collabkit/client/src/utils/signInWithUserToken.ts
+++ packages/@collabkit/client/src/utils/signInWithUserToken.ts
@@ -1,20 +1,23 @@
 import { API_HOST } from '../constants';
 
-export async function signInWithUserToken(appId:string, userToken:string){
-    const response = await fetch(`https://us-central1-collabkit-test.cloudfunctions.net/generateCustomToken`, {
-        method: 'POST',
-        body: JSON.stringify({
-            appId: appId,
-            userToken: userToken
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-    });
-    if(response.ok){
-        const json = await response.json();
-        return json.data.token;
-    }else{
-        console.error('Failed to generateCustomToken', response.status, await response.text());
+export async function signInWithUserToken(appId: string, userToken: string) {
+  const response = await fetch(
+    `${API_HOST}/generateCustomToken`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        appId: appId,
+        userToken: userToken,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
+  );
+  if (response.ok) {
+    const json = await response.json();
+    return json.data.token;
+  } else {
+    console.error('Failed to generateCustomToken', response.status, await response.text());
+  }
 }
