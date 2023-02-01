4724f8e29f02bd749300069864f51aca9f50ea39
Meet Shah <meetcshah19@gmail.com>
2023-02-01 19:28:37 +0530

    add docs

--- www/src/docs/SecureModeDoc.tsx
+++ www/src/docs/SecureModeDoc.tsx
@@ -11,7 +11,7 @@ export function SecureModeDoc() {
       <div>
         <h2>Make sure you turn this on before shipping to production</h2>
         <p>
-          Secure Mode requires fetching a per user token and passing it to the client. This keeps
+          Secure Mode requires signing a per user token and passing it to the client. This keeps
           your <code>apiKey</code> safe and prevents anyone from writing or reading comments
           indended for other users or workspaces.
         </p>
@@ -24,28 +24,32 @@ export function SecureModeDoc() {
         <p>Email or Slack us and we'll enable Secure Mode for your app.</p>
       </div>
       <div className={inset}>
-        <h3 className={docStep}>Generate a per-user token</h3>
+        <h3 className={docStep}>Create a per-user token</h3>
         <div>
           <h4>For Node.js</h4>
           <p>
-            Install <code>@collabkit/node</code> and call <code>getUserToken</code>.
+            Install <code>@collabkit/node</code> and call <code>createUserToken</code>.
           </p>
           {renderCodeSnippet(SecureModeDocNodeSnippet)}
+          <br />
+
+          <h4>For Python</h4>
+          <p>
+            Install <a href='https://pyjwt.readthedocs.io/en/stable/index.html'>PyJWT</a> and use the following python code:
+            <br />
+            {renderCodeSnippet('import jwt \ntoken = jwt.encode({ "userId": "<your user ID here>", "workspaceId": "<your workspaceId here>" }, "<your apiKey here>", algorithm="HS256")')}
+          </p>
           <br />
           <h4>For everything else (HTTP request)</h4>
 
           <p>
-            Generate a token by making a <code>POST</code> request to{' '}
-            <code>https://token.collabkit.dev</code> with the following JSON payload. <br />
-            <br />
+            Create a user token by createing a JWT token with the following payload signed by your API key as the secret using the HS256 algorithm:
             {renderCodeSnippet(SecureModeDocJsonSnippet)}
-            <p>You'll receive a response of the format:</p>
-            {renderCodeSnippet(SecureModeDocResponseSnippet)}
           </p>
         </div>
       </div>
       <div className={inset}>
-        <h3 className={docStep}>Pass the token to the CollabKitProvider on the client</h3>
+        <h3 className={docStep}>Pass the user token to the CollabKitProvider on the client</h3>
         {renderCodeSnippet(SecureModeDocReactSnippet)}
       </div>
       <div className={inset}>
--- www/src/docs/SecureModeDocNodeSnippet.tsx
+++ www/src/docs/SecureModeDocNodeSnippet.tsx
@@ -1,24 +1,7 @@
-import { getUserToken } from '@collabkit/node';
+import { createUserToken } from '@collabkit/node';
 
-const response = await getUserToken({
-  appId: 'your APP ID here',
-  apiKey: 'your API Key here',
-  userId: 'jane',
-  workspaceId: 'acme',
-  user: {
-    name: 'Jane Doe', // optional
-    email: 'jane@example.com', // optional
-    avatar: 'https://example.com/jane.jpg', // optional
-  },
-  workspace: {
-    name: 'ACME Corporation', // optional
-  },
-});
-
-let token;
-if (response?.status === 201) {
-  token = response.data.token;
-  // pass token to client
-} else {
-  throw new Error('Failed to generate token');
-}
+const token = createUserToken(
+  '<your API Key here>',
+  '<your user ID here>',
+  '<your workspace ID here>'
+);
