fbb1fc754a0cb6975eecb25a3d3332295d811976
Namit Chadha <namit.chadha@gmail.com>
2023-02-04 03:05:14 +0000

    add missing prop

--- packages/@collabkit/react/src/components/ReactionPicker.stories.tsx
+++ packages/@collabkit/react/src/components/ReactionPicker.stories.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { ReactionPicker as ReactPickerComponent } from './ReactionPicker';
 
 export function ReactionPicker() {
-  return <ReactPickerComponent />;
+  return <ReactPickerComponent onClick={() => {}} />;
 }
