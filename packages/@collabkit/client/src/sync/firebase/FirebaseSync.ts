336b0de0a25fcf8c74d7a61931045bedd68294d7
Namit Chadha <namit.chadha@gmail.com>
2023-02-05 17:13:28 +0000

    remove log statement

--- packages/@collabkit/client/src/sync/firebase/FirebaseSync.ts
+++ packages/@collabkit/client/src/sync/firebase/FirebaseSync.ts
@@ -146,7 +146,6 @@ export class FirebaseSync implements Sync.SyncAdapter {
       threadId: pin.threadId,
       createdById: userId,
     };
-    console.log(firebasePin);
     const updates = {
       [ref.path`/pins/${appId}/${workspaceId}/${objectId}/${pinId}`]: firebasePin,
       [ref.path`/views/openPins/${appId}/${workspaceId}/${objectId}/${pinId}`]: firebasePin,
