43af1678a9cf80e5c545eb85dd2605ab1c1191da
Meet Shah <meetcshah19@gmail.com>
2023-02-06 16:51:02 +0530

    v1 namespace

--- functions/src/routes.ts
+++ functions/src/routes.ts
@@ -15,30 +15,34 @@ export const routes = functions.https.onRequest(async (request, response) => {
   corsHandler(request, response, async () => {
     const url = new URL(request.url, `http://${request.headers.host}`);
 
-    switch (url.pathname.split('/')[1]) {
-      case 'generateCustomToken':
-        generateCustomTokenImpl(request, response);
-        break;
-      case 'createUser':
-        createUserImpl(request, response);
-        break;
-      case 'createWorkspace':
-        createWorkspaceImpl(request, response);
-        break;
-      case 'createOrg':
-        createOrgImpl(request, response);
-        break;
-      case 'comment':
-        commentImpl(request, response);
-        break;
-      case 'sendNotification':
-        sendNotificationImpl(request, response);
-        break;
-      case 'triggerWebhook':
-        triggerWebhookImpl(request, response);
-        break;
-      default:
-        response.status(404).send('Route not found');
+    if (url.pathname.split('/')[0] === 'v1') {
+      switch (url.pathname.split('/')[1]) {
+        case 'generateCustomToken':
+          generateCustomTokenImpl(request, response);
+          break;
+        case 'createUser':
+          createUserImpl(request, response);
+          break;
+        case 'createWorkspace':
+          createWorkspaceImpl(request, response);
+          break;
+        case 'createOrg':
+          createOrgImpl(request, response);
+          break;
+        case 'comment':
+          commentImpl(request, response);
+          break;
+        case 'sendNotification':
+          sendNotificationImpl(request, response);
+          break;
+        case 'triggerWebhook':
+          triggerWebhookImpl(request, response);
+          break;
+        default:
+          response.status(404).send('Route not found');
+      }
+    } else {
+      response.status(404).send('Route not found');
     }
   });
 });
