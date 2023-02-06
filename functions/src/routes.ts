389aa43e5ae7060a881960991ee14e7eff92854a
Meet Shah <meetcshah19@gmail.com>
2023-02-06 17:23:18 +0530

    fix route test

--- functions/src/routes.ts
+++ functions/src/routes.ts
@@ -11,38 +11,40 @@ import { triggerWebhookImpl } from './triggerWebhook';
 
 const corsHandler = cors.default({ origin: true });
 
-export const routes = functions.https.onRequest(async (request, response) => {
-  corsHandler(request, response, async () => {
-    const url = new URL(request.url, `http://${request.headers.host}`);
+export const routesImpl = (request: functions.https.Request, response: functions.Response) => {
+  const url = new URL(request.url, `http://${request.headers.host}`);
 
-    if (url.pathname.split('/')[0] === 'v1') {
-      switch (url.pathname.split('/')[1]) {
-        case 'generateCustomToken':
-          generateCustomTokenImpl(request, response);
-          break;
-        case 'createUser':
-          createUserImpl(request, response);
-          break;
-        case 'createWorkspace':
-          createWorkspaceImpl(request, response);
-          break;
-        case 'createOrg':
-          createOrgImpl(request, response);
-          break;
-        case 'comment':
-          commentImpl(request, response);
-          break;
-        case 'sendNotification':
-          sendNotificationImpl(request, response);
-          break;
-        case 'triggerWebhook':
-          triggerWebhookImpl(request, response);
-          break;
-        default:
-          response.status(404).send('Route not found');
-      }
-    } else {
-      response.status(404).send('Route not found');
+  if (url.pathname.split('/')[0] === 'v1') {
+    switch (url.pathname.split('/')[1]) {
+      case 'generateCustomToken':
+        generateCustomTokenImpl(request, response);
+        break;
+      case 'createUser':
+        createUserImpl(request, response);
+        break;
+      case 'createWorkspace':
+        createWorkspaceImpl(request, response);
+        break;
+      case 'createOrg':
+        createOrgImpl(request, response);
+        break;
+      case 'comment':
+        commentImpl(request, response);
+        break;
+      case 'sendNotification':
+        sendNotificationImpl(request, response);
+        break;
+      case 'triggerWebhook':
+        triggerWebhookImpl(request, response);
+        break;
+      default:
+        response.status(404).send('Route not found');
     }
-  });
+  } else {
+    response.status(404).send('Route not found');
+  }
+};
+
+export const routes = functions.https.onRequest(async (request, response) => {
+  corsHandler(request, response, async () => {});
 });
--- functions/src/test/routing.test.ts
+++ functions/src/test/routing.test.ts
@@ -4,7 +4,7 @@ import * as functions from 'firebase-functions';
 
 import { routes } from '../routes';
 
-const mockHttp = (props: { query?: object; body?: object; headers?: object }) => {
+const mockHttp = (props: { url?: string; query?: object; body?: object; headers?: object }) => {
   const req = { headers: { origin: '' }, query: {}, body: {}, ...props } as functions.https.Request;
 
   return {
@@ -23,4 +23,12 @@ it('routing: route not found', async () => {
   const send = http.res.send as sinon.SinonSpy;
   const { args } = send.getCalls()[0];
   expect(args[0]).toEqual({ status: 404, error: 'Route not found' });
+});
+
+it('routing: route works', async () => {
+    const http = mockHttp({ url: "/v1/createUser/", query: {}, body: {} });
+    await routes(http.req, http.res);
+    const send = http.res.send as sinon.SinonSpy;
+    const { args } = send.getCalls()[0];
+    expect(args[0]).toEqual({ status: 400, error: '"appId" not provided' });
 });
