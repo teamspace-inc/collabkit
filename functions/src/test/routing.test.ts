183d6dc0d7605de85a41cb0577148d8fb430d7ca
Meet Shah <meetcshah19@gmail.com>
2023-02-06 16:58:19 +0530

    add route test

---
+++ functions/src/test/routing.test.ts
@@ -0,0 +1,26 @@
+import sinon from 'sinon';
+
+import * as functions from 'firebase-functions';
+
+import { routes } from '../routes';
+
+const mockHttp = (props: { query?: object; body?: object; headers?: object }) => {
+  const req = { headers: { origin: '' }, query: {}, body: {}, ...props } as functions.https.Request;
+
+  return {
+    req: req,
+    res: {
+      status: sinon.stub().returnsThis(),
+      send: sinon.stub().returnsThis(),
+      set: sinon.stub().returnsThis(),
+    } as unknown as functions.Response<any>,
+  };
+};
+
+it('routing: route not found', async () => {
+  const http = mockHttp({ query: {}, body: {} });
+  await routes(http.req, http.res);
+  const send = http.res.send as sinon.SinonSpy;
+  const { args } = send.getCalls()[0];
+  expect(args[0]).toEqual({ status: 404, error: 'Route not found' });
+});
