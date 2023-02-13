1cd63d7904f203070473885b3a4134fa941d6a8c
Namit Chadha <namit.chadha@gmail.com>
2023-02-13 15:43:14 +0000

    app tsx

--- examples/demo/src/App.tsx
+++ examples/demo/src/App.tsx
@@ -71,6 +71,76 @@ export default function App() {
   );
 }
 
+// subscriber
+// createUserToken({ apiKey, userId, workspaceId })
+//
+
+function TellaDemo() {
+  const { user } = useSnapshot(store);
+  const { apiKey, appId, workspaceId, workspaceName } = useAppParams();
+  const test = useTestParams();
+  const [pathname] = useLocation();
+  const name = pathname.slice(1);
+  const theme: CustomTheme | undefined =
+    name in themes ? themes[name as keyof typeof themes] : undefined;
+  return (
+    <CollabKitProvider
+      _test={test}
+      apiKey={apiKey}
+      appId={appId}
+      workspace={{ id: workspaceId, name: workspaceName }}
+      callbacks={{
+        onTimestampClick: (data) => {
+          console.log('timestamp, click', data);
+        },
+        onThreadCreated: (data) => {
+          console.log('thread, created', data);
+        },
+        // onCommentSend: (data) => {
+        //   console.log(data);
+        // },
+        // onMentionClick: (data) => {
+        //   console.log('mention, click', data);
+        // },
+
+        // onInboxCloseButtonClick: (data) => {
+        //   console.log('inbox close button, click', data);
+        // },
+        // onThreadResolve: (data) => console.log('resolve', data),
+        // onThreadReopen: (data) => console.log('reopen', data),
+      }}
+      onAuthenticationRequired={() => {
+        console.log('authRequired');
+      }}
+      // warning: this is a hack
+      // this is the strangest thing, if we pass a snapshot into our product
+      // it breaks our app, but if we stringify and then parse it, it works
+      user={JSON.parse(JSON.stringify(user))}
+      theme={theme}
+      // theme="dark"
+      // renderAvatar={CustomAvatar}
+      // renderThreadContextPreview={() => {
+      //   return (
+      //     <div
+      //       style={{
+      //         width: '100%',
+      //         height: 38,
+      //         background: '#F5F8FA',
+      //         borderRadius: 8,
+      //       }}
+      //     ></div>
+      //   );
+      // }}
+      mentionableUsers={'allWorkspace'}
+    >
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/logout" component={Logout} />
+      </Switch>
+    </CollabKitProvider>
+  );
+}
+
 function Demo() {
   const { user } = useSnapshot(store);
   const { apiKey, appId, workspaceId, workspaceName } = useAppParams();
