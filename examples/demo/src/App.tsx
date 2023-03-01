24cabbffed8316903805eb49690f6f64dc7cf4c9
Ville Immonen <ville.immonen@iki.fi>
2023-03-01 19:28:10 +0200

    Fix demo authentication

--- examples/demo/src/App.tsx
+++ examples/demo/src/App.tsx
@@ -3,14 +3,60 @@ import { Demo } from './Demo';
 import { useAppParams } from './hooks/useAppParams';
 import { SignIn } from './SignIn';
 import { store } from './store';
+import useLocalStorage from 'react-use/lib/useLocalStorage';
+import { useEffect } from 'react';
 
 export default function App() {
+  const { appId, token, setCredential, isLoading } = useUserToken();
+
+  if (isLoading) return null;
+
+  if (token) {
+    return <Demo appId={appId} token={token} />;
+  } else {
+    return (
+      <SignIn
+        onSuccess={(credentialResponse) => {
+          const { credential } = credentialResponse;
+          if (credential) {
+            setCredential(credential);
+          }
+        }}
+      />
+    );
+  }
+}
+
+function useUserToken() {
+  const [credential, setCredential, removeCredential] = useLocalStorage<string>('credential');
   const snapshot = useSnapshot(store);
   const params = useAppParams();
   const token = params.token ?? snapshot.token;
-  if (token) {
-    return <Demo appId={params.appId} token={token} />;
-  } else {
-    return <SignIn />;
-  }
+  const shouldFetchToken = !token && !!credential;
+
+  useEffect(() => {
+    console.log({ token, credential });
+    if (shouldFetchToken) {
+      const authenticate = async () => {
+        try {
+          const response = await fetch(`${import.meta.env.VITE_API_HOST}/api/authenticate`, {
+            method: 'POST',
+            body: JSON.stringify({ credential }),
+            headers: {
+              'content-type': 'application/json',
+            },
+          });
+          if (!response.ok) throw new Error('Failed to authenticate');
+          const result = await response.json();
+          store.token = result.token;
+        } catch (e) {
+          console.error(e);
+          removeCredential();
+        }
+      };
+      authenticate();
+    }
+  }, [token, credential]);
+
+  return { appId: params.appId, token, setCredential, isLoading: shouldFetchToken };
 }
