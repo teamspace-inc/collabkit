5c0d3f14a2ce7cfb8c601a9b17ee67cc59c4b2a3
Namit Chadha <namit.chadha@gmail.com>
2023-03-06 11:08:11 +0000

    dont wait for auth before opening sidebar

--- packages/@collabkit/react/src/hooks/public/useSidebarButton.ts
+++ packages/@collabkit/react/src/hooks/public/useSidebarButton.ts
@@ -1,31 +1,23 @@
 import { ToggleSidebarButtonTarget } from '@collabkit/core';
 import { useSnapshot } from 'valtio';
 import { useApp } from '../useApp';
-import { useIsAuthenticated } from '../useIsAuthenticated';
 
 export function useSidebarButton() {
   const { store, events } = useApp();
-  const isAuthenticated = useIsAuthenticated();
   const { workspaceId } = useSnapshot(store);
 
-  return isAuthenticated
-    ? {
-        onClick: (e: React.MouseEvent) => {
-          if (workspaceId) {
-            const target: ToggleSidebarButtonTarget = {
-              type: 'toggleSidebarButton',
-            };
-            if (e.button === 0) {
-              events.onClick(e, { target });
-            }
-          } else {
-            console.error('[CollabKit] Workspace ID not found');
-          }
-        },
+  return {
+    onClick: (e: React.MouseEvent) => {
+      if (workspaceId) {
+        const target: ToggleSidebarButtonTarget = {
+          type: 'toggleSidebarButton',
+        };
+        if (e.button === 0) {
+          events.onClick(e, { target });
+        }
+      } else {
+        console.error('[CollabKit] Workspace ID not found');
       }
-    : {
-        onClick: (e: React.MouseEvent) => {
-          console.warn('CollabKit: cant show sidebar while user is not authenticated');
-        },
-      };
+    },
+  };
 }
