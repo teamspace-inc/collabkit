6f0b06b230ebb741205819a7d8537b25015829c9
Namit Chadha <namit.chadha@gmail.com>
2023-03-06 09:21:11 +0000

    fix floating composer bug

--- packages/@collabkit/client/src/actions/deselectAll.ts
+++ packages/@collabkit/client/src/actions/deselectAll.ts
@@ -3,7 +3,8 @@ import { Store } from '@collabkit/core';
 export function deselectAll(store: Store) {
   if (store.selectedId?.type === 'pin') {
     const pin = store.workspaces[store.selectedId.workspaceId].eventPins[store.selectedId.eventId];
-    if (!pin.meta) return;
+    // this means it's a pending pin
+    if (!pin) return;
     store.callbacks?.onPinDeselect?.({
       userId: store.userId!,
       objectId: store.selectedId.objectId,
