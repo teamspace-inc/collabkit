cec80a77ba06d3d9a5d99e63c3a6f807f77b6cbf
Namit Chadha <namit.chadha@gmail.com>
2023-03-06 13:22:41 +0000

    dont wait for workspaceId for toggleButton

--- packages/@collabkit/react/src/hooks/public/useSidebarButton.ts
+++ packages/@collabkit/react/src/hooks/public/useSidebarButton.ts
@@ -1,20 +1,16 @@
 import { ToggleSidebarButtonTarget } from '@collabkit/core';
-import { useSnapshot } from 'valtio';
 import { useApp } from '../useApp';
 
 export function useSidebarButton() {
-  const { store, events } = useApp();
-  const { workspaceId } = useSnapshot(store);
+  const { events } = useApp();
 
   return {
     onClick: (e: React.MouseEvent) => {
-      if (workspaceId) {
-        const target: ToggleSidebarButtonTarget = {
-          type: 'toggleSidebarButton',
-        };
-        if (e.button === 0) {
-          events.onClick(e, { target });
-        }
+      const target: ToggleSidebarButtonTarget = {
+        type: 'toggleSidebarButton',
+      };
+      if (e.button === 0) {
+        events.onClick(e, { target });
       } else {
         console.error('[CollabKit] Workspace ID not found');
       }
