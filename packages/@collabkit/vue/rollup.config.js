3f1ccba828456d15965ac7a050783440cb18a759
Ville Immonen <ville.immonen@iki.fi>
2023-03-09 16:25:07 +0200

    Make @collabkit/react external in the vue package

--- packages/@collabkit/vue/rollup.config.js
+++ packages/@collabkit/vue/rollup.config.js
@@ -5,7 +5,7 @@ const config = [
     input: './types/vue/src/index.d.ts',
     output: [{ file: 'dist/collabkit-vue.d.ts', format: 'es' }],
     plugins: [dts({ respectExternal: true })],
-    external: ['vue', 'react', 'date-fns', 'date-fns/locale'],
+    external: ['@collabkit/react', 'vue', 'react', 'date-fns', 'date-fns/locale'],
   },
 ];
 
