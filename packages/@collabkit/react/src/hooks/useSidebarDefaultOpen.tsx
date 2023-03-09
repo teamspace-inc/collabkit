76f63440efd5f9f2674d18df5cc36184a204f949
Namit Chadha <namit.chadha@gmail.com>
2023-03-09 13:09:45 +0000

    prop type

--- packages/@collabkit/react/src/hooks/useSidebarDefaultOpen.tsx
+++ packages/@collabkit/react/src/hooks/useSidebarDefaultOpen.tsx
@@ -2,7 +2,7 @@ import { actions } from '@collabkit/client';
 import { useEffect } from 'react';
 import { useStore } from '../hooks/useStore';
 
-export function useSidebarDefaultOpen(props: { defaultOpen?: any }) {
+export function useSidebarDefaultOpen(props: { defaultOpen?: boolean }) {
   const { defaultOpen } = props;
   const store = useStore();
   useEffect(() => {
