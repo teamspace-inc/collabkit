0589b33e167a2b303129f7aecdf4ce2af2c1c8ec
Namit Chadha <namit.chadha@gmail.com>
2023-03-12 16:33:34 +0000

    make SidebarComments width default but customisable via wrapper

--- packages/@collabkit/react/src/components/SidebarComments.tsx
+++ packages/@collabkit/react/src/components/SidebarComments.tsx
@@ -5,12 +5,13 @@ import { ChannelNewThreadComposer, ChannelRoot, ChannelScrollableThreadList } fr
 import { SidebarRoot, SidebarHeader, SidebarTitle, SidebarCloseButton } from './Sidebar';
 import { useSidebarDefaultOpen } from '../hooks/useSidebarDefaultOpen';
 import { ThemeWrapper } from './ThemeWrapper';
+import { wrapper } from '../theme/components/SidebarComments.css';
 
 function SidebarComments(props: React.ComponentPropsWithoutRef<'div'> & { defaultOpen?: boolean }) {
   const { defaultOpen, ...otherProps } = props;
   useSidebarDefaultOpen(props);
   return useIsSidebarOpen() ? (
-    <div {...otherProps}>
+    <div className={wrapper} {...otherProps}>
       <ThemeWrapper>
         <Authenticated>
           <SidebarRoot>
