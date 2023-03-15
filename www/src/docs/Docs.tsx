f736ad2677c60c8181ca8ad602abd7b773e83165
Ville Immonen <ville.immonen@iki.fi>
2023-03-15 13:12:03 +0200

    Mirror the site structure in the folder structure

--- www/src/docs/Docs.tsx
+++ www/src/docs/Docs.tsx
@@ -4,33 +4,22 @@ import Workspaces from './Workspaces.mdx';
 import Notifications from './Notifications.mdx';
 import SidebarComments from './components/SidebarComments.mdx';
 import Provider from './components/Provider.mdx';
-import CollabKitVueProvider from './components/vue/CollabKitVueProvider.mdx';
-import CollabKitContextProvider from './components/vue/CollabKitContextProvider.mdx';
-import VueCreateCollabKit from './components/vue/createCollabKit.mdx';
+import CollabKitVueProvider from './vue/CollabKitVueProvider.mdx';
+import CollabKitContextProvider from './vue/CollabKitContextProvider.mdx';
+import VueCreateCollabKit from './vue/createCollabKit.mdx';
 import Thread from './components/Thread.mdx';
 import Commentable from './components/Commentable.mdx';
 import Themes from './Themes.mdx';
 
 import { RootDocNode } from './DocRoutes';
-import { UpsertUserDoc } from './components/nodeClient/upsertUserDoc';
-import { UpsertWorkspaceDoc } from './components/nodeClient/upsertWorkspaceDoc';
-import { DeleteUserDoc } from './components/nodeClient/deleteUserDoc';
+import UpsertUserDoc from './node/upsertUser.mdx';
+import UpsertWorkspaceDoc from './node/upsertWorkspace.mdx';
+import DeleteUserDoc from './node/deleteUser.mdx';
 import AllComponents from './components/AllComponents.mdx';
 import Navigation from './Navigation.mdx';
 
 export const DOCS: RootDocNode = {
-  // Dashboard: { component: DashboardPage },
-  // Empty: { isEmpty: true },
   'Getting Started': { component: GettingStarted },
-  // 'Design Patterns': { component: DesignPatterns },
-  // Patterns: {
-  //   title: 'Patterns',
-  //   children: {
-  //     'Detail Views': { component: DetailViewsDoc },
-  //     'List Views': { component: ListViewsDoc },
-  //     'Table Views': { component: TableViewsDoc },
-  //   },
-  // },
   Core: {
     title: 'Core',
     children: {
@@ -48,52 +37,20 @@ export const DOCS: RootDocNode = {
       Overview: { component: AllComponents },
       SidebarComments: { component: SidebarComments },
       Commentable: { component: Commentable },
-      // Inbox: { component: InboxDoc },
-      // InboxButton: { component: InboxButtonDoc },
-      // SidebarInbox: { component: SidebarInboxDoc },
-      // SidebarInboxButton: { component: SidebarInboxButtonDoc },
-      // Avatar: { component: AvatarDoc },
-      // Facepile: { component: FacepileDoc },
     },
   },
-  // Hooks: {
-  //   title: 'Hooks',
-  //   children: {
-  //     // useUnreadCommentsCount: { component: UseUnreadCommentsCountDoc },
-  //     // useUnreadThreadsCount: { component: UseUnreadThreadsCountDoc },
-  //   },
-  // },
-  // 'Api Reference': {
-  //   title: 'HTTP API',
-  //   children: {
-  //     CreateComment: { component: CommentAPI },
-  //   },
-  // },
   Other: {
     title: 'Other',
     children: {
       Thread: { component: Thread },
-      // Inbox: { component: InboxDoc },
-      // InboxButton: { component: InboxButtonDoc },
-      // SidebarInbox: { component: SidebarInboxDoc },
-      // SidebarInboxButton: { component: SidebarInboxButtonDoc },
-      // Avatar: { component: AvatarDoc },
-      // Facepile: { component: FacepileDoc },
     },
   },
-  // 'Api Reference': {
-  //   title: 'HTTP API',
-  //   children: {
-  //     CreateComment: { component: CommentAPI },
-  //   },
-  // },
-  'Node client': {
+  Node: {
     title: 'Node',
     children: {
       deleteUser: { component: DeleteUserDoc },
       upsertUser: { component: UpsertUserDoc },
       upsertWorkspace: { component: UpsertWorkspaceDoc },
-      // createUserToken: {component: createUserToken}
     },
   },
   Vue: {
--- www/src/docs/SecureModeDocNodeSnippet.tsx
+++
@@ -1,7 +0,0 @@
-import { createUserToken } from '@collabkit/node';
-
-const token = createUserToken({
-  apiKey: '<your API Key here>',
-  userId: '<your user ID here>',
-  workspaceId: '<your workspace ID here>'
-});
