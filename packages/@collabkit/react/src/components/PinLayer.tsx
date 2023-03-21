7b12fc0997b59fcb498e8d6bca78b192e49eb8f4
Omar Thanawalla <omar.thanawalla@gmail.com>
2023-03-21 15:56:47 +0000

    Remove mouse on uncommentable hovered items

--- packages/@collabkit/react/src/components/PinLayer.tsx
+++ packages/@collabkit/react/src/components/PinLayer.tsx
@@ -13,7 +13,9 @@ import { findCommentableElement } from './Commentable';
 function PinLayer(props: { className?: string; children?: React.ReactNode }) {
   const cursorRef = useRef<HTMLDivElement | null>(null);
   const overlayRef = useRef<HTMLDivElement | null>(null);
-  const hoveredElementRef = useRef<HTMLElement | SVGElement | null>(null);
+  const hoveredCommentableElementRef = useRef<HTMLElement | SVGElement | null>(null);
+  const hoveredUncommentableElementRef = useRef<Element | SVGElement | null>(null);
+
   const store = useStore();
   const { events } = useApp();
   const { userId, uiState, workspaceId, pins, selectedId, pinsVisible } = useSnapshot(store);
@@ -27,13 +29,19 @@ function PinLayer(props: { className?: string; children?: React.ReactNode }) {
 
   const updateCursor = useCallback(
     (e: PointerEvent) => {
-      if (hoveredElementRef.current) {
-        hoveredElementRef.current.classList.remove(styles.activeContainer);
-        hoveredElementRef.current = null;
+      if (hoveredCommentableElementRef.current) {
+        hoveredCommentableElementRef.current.classList.remove(styles.activeContainer);
+        hoveredCommentableElementRef.current = null;
       }
+      if (hoveredUncommentableElementRef.current) {
+        hoveredUncommentableElementRef.current.classList.remove(styles.activeContainer);
+        hoveredUncommentableElementRef.current = null;
+      }
+
       if (!cursorRef.current || !overlayRef.current) {
         return;
       }
+
       cursorRef.current.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
       store.clientX = e.clientX;
       store.clientY = e.clientY;
@@ -43,7 +51,7 @@ function PinLayer(props: { className?: string; children?: React.ReactNode }) {
       if (commentable && commentable.element) {
         const { element } = commentable;
         element.classList.add(styles.activeContainer);
-        hoveredElementRef.current = element;
+        hoveredCommentableElementRef.current = element;
         cursorRef.current.style.opacity = '1.0';
         const { left, top, width, height } = element.getBoundingClientRect();
         overlayRef.current.style.left = `${left}px`;
@@ -54,6 +62,12 @@ function PinLayer(props: { className?: string; children?: React.ReactNode }) {
       } else {
         cursorRef.current.style.opacity = '0.4';
         overlayRef.current.style.display = 'none';
+        const element = document.elementFromPoint(e.clientX, e.clientY);
+        if (element) {
+          element.classList.add(styles.activeContainer);
+          hoveredUncommentableElementRef.current = element;
+        }
+
       }
     },
     [cursorRef, store]
