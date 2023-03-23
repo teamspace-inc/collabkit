df06ccd07d34deee4f0f23db17e2221b4fd0c426
Omar Thanawalla <omar@useteamspace.com>
2023-03-23 13:18:05 +0000

    Add edge function package to monorepo

---
+++ packages/@collabkit/edge/pages/api/completions.js
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export const config = {
+    runtime: 'edge',
+};
+
+export default (req) => {
+    return NextResponse.json({
+        name: `Hello, from ${req.url} I'm now an Edge Function!`,
+    });
+};
