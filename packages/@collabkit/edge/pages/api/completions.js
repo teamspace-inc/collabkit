d7532d2f0db9c3bda5d8e144b642ab369286ccfc
Omar Thanawalla <omar@useteamspace.com>
2023-03-23 14:06:09 +0000

    Code review changes

--- packages/@collabkit/edge/pages/api/completions.js
+++ packages/@collabkit/edge/pages/api/completions.js
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server';
 
 export const config = {
-    runtime: 'edge',
+  runtime: 'edge',
 };
 
 export default (req) => {
-    return NextResponse.json({
-        name: `Hello, from ${req.url} I'm now an Edge Function!`,
-    });
+  return NextResponse.json({
+    name: `Hello, from ${req.url} I'm now an Edge Function!`,
+  });
 };
