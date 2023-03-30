3749359bd7fd45c54d22291b97ce81439f15a7e4
Omar Thanawalla <omar@useteamspace.com>
2023-03-30 15:43:33 +0100

    checkpoint, trying to debug use state problem

--- packages/@collabkit/edge2/pages/index.tsx
+++ packages/@collabkit/edge2/pages/index.tsx
@@ -1,4 +1,5 @@
 import { AnimatePresence, motion } from 'framer-motion';
+
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -14,20 +15,68 @@ import ResizablePanel from '../components/ResizablePanel';
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
   const [bio, setBio] = useState('');
-  const [generatedBios, setGeneratedBios] = useState<String>('');
+  const [generatedBios, setGeneratedBios] = useState(false);
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
-      
-      
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        
-        
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Twitter bio using chatGPT
+          Generate your next Twitter Github Issues LLM
         </h1>
+        <div className="max-w-xl w-full">
+          <div className="flex mt-10 items-center space-x-3">
+            <Image
+              src="/1-black.png"
+              width={30}
+              height={30}
+              alt="1 icon"
+              className="mb-5 sm:mb-0"
+            />
+            <p className="text-left font-medium">
+              Copy your current bio{' '}
+              <span className="text-slate-500">(or write a few sentences about yourself)</span>.
+            </p>
+          </div>
 
+          <textarea
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
+            rows={4}
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            placeholder={
+              'e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com.'
+            }
+          />
+          {!loading && (
+            <button className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full">
+              Generate your bio &rarr;
+            </button>
+          )}
+          {loading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              disabled
+            >
+              <LoadingDots color="white" style="large" />
+            </button>
+          )}
+        </div>
 
+        <Toaster position="top-center" reverseOrder={false} toastOptions={{ duration: 2000 }} />
+
+        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+
+        <div className="space-y-10 my-10">
+          {generatedBios && (
+            <>
+              <div>
+                <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
+                  Your generated bios
+                </h2>
+              </div>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
