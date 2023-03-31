bd2adb8eb55bac7cc82fbabc910650962148fe27
Omar Thanawalla <omar@useteamspace.com>
2023-03-31 13:13:52 +0100

    Remove import statements

--- examples/demo-api/pages/index.tsx
+++ examples/demo-api/pages/index.tsx
@@ -2,10 +2,6 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useState } from 'react';
-import DropDown, { VibeType } from '../components/DropDown';
-import Footer from '../components/Footer';
-import Github from '../components/GitHub';
-import Header from '../components/Header';
 import LoadingDots from '../components/LoadingDots';
 
 const Home: NextPage = () => {
