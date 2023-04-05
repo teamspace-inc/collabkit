a9b6b799bab8392f6deb518b66caba6f0f805dd5
Omar Thanawalla <omar@useteamspace.com>
2023-04-05 15:22:15 +0100

    Respond with Agents response

--- papi/api/bq.py
+++ papi/api/bq.py
@@ -1,3 +1,6 @@
+import os
+os.environ["OPENAI_API_KEY"] = ""
+
 from fastapi import FastAPI
 from sqlalchemy import *
 from sqlalchemy.engine import create_engine
@@ -9,14 +12,8 @@ from langchain.llms.openai import OpenAI
 from langchain.agents import AgentExecutor
 
 app = FastAPI()
-print("running api/index")
-
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/omar")
 async def hello_message():
   uri = "bigquery://bigquerysandboxproject-382616/covid19_nyt"
 
@@ -33,10 +30,5 @@ async def hello_message():
       toolkit=toolkit,
       verbose=True
   )
-  agent_executor.run("Which state had the highest covid cases?")
-  return {"message": "Agent ran"}
-
-
-
-
-
+  agentResponse = agent_executor.run("Which state had the highest covid cases?")
+  return {"message": agentResponse}
