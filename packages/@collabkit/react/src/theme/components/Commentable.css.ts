3234b7173e213aa70eb6e169a4f8da014d6b52de
Ville Immonen <ville.immonen@iki.fi>
2023-04-05 18:22:49 +0300

    Remove unused style

--- packages/@collabkit/react/src/theme/components/Commentable.css.ts
+++ packages/@collabkit/react/src/theme/components/Commentable.css.ts
@@ -7,14 +7,6 @@ globalStyle(`${selecting} *`, {
   cursor: 'none !important',
 });
 
-export const cursor = style({
-  position: 'absolute',
-  width: 24,
-  height: 24,
-  background: 'yellow',
-  pointerEvents: 'none',
-});
-
 export const overlay = style({
   position: 'absolute',
   pointerEvents: 'none',
