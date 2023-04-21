621d15903ef1dabdf8e02e5e355fbf2c66380ed9
Namit Chadha <namit.chadha@gmail.com>
2023-04-21 10:24:50 +0100

    dashboard

---
+++ shape-sql/src/app/(auth)/setup/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function Page({ params }: { params: { id: string } }) {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
