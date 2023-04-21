2c47043ca4d05a4848b143cc205106e5c5726ded
Namit Chadha <namit.chadha@gmail.com>
2023-04-21 11:49:24 +0100

    token

--- shape-sql/src/app/(auth)/setup/page.tsx
+++ shape-sql/src/app/(auth)/setup/page.tsx
@@ -1,14 +1,30 @@
 'use client';
+import { Button } from '@/app/Button';
 import { useUser } from '@clerk/nextjs';
 
 export default function Page({ params }: { params: { id: string } }) {
   const user = useUser();
+  const botToken = user ? (user.user?.publicMetadata['botToken'] as string) : null;
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1>Dashboard</h1>
-      <div>
-        <h1>Slack Key:</h1>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div
+        style={{
+          maxWidth: 960,
+          height: 400,
+          display: 'flex',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+        }}
+      >
+        <h1>Dashboard</h1>
+        <div>{botToken ?? <Button>Connect Slack</Button>}</div>
       </div>
     </div>
   );
