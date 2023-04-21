27b4a34caf35a7bef0b89196186c94ecde983f85
Namit Chadha <namit.chadha@gmail.com>
2023-04-21 16:28:49 +0100

    Dashbaord (#345)
    * auth basics
    
    * put middleware in the right folder
    
    * ui
    
    * sign in page
    
    * fix home styling
    
    * dashboard
    
    * org picker, styles
    
    * styles
    
    * remove import
    
    * x
    
    * token
    
    * we
    
    * Meet/add to slack (#347)
    
    * checkpoint
    
    * add slack button
    
    * dashboard connect
    
    * dashboard layout
    
    ---------
    
    Co-authored-by: Meet Shah <36073333+meetcshah19@users.noreply.github.com>

--- shape-sql/src/app/(auth)/layout.tsx
+++ shape-sql/src/app/(auth)/layout.tsx
@@ -1,13 +1,25 @@
 'use client';
-import { UserButton } from '@clerk/nextjs';
-import styles from '@/app/Nav.module.css';
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 import { Logo } from '@/app/Logo';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <nav className={styles.nav}>
+      <nav
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          height: '80px',
+          display: 'grid',
+          padding: '20px 20px',
+          margin: '0 auto',
+          gridTemplateColumns: '6fr 240px 30px',
+        }}
+      >
         <Logo size="M" />
+        <OrganizationSwitcher />
         <UserButton />
       </nav>
       {children}
