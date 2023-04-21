7a68c56468fbba0967420a513ff2f2aebc78a495
Namit Chadha <namit.chadha@gmail.com>
2023-04-21 11:33:40 +0100

    x

--- shape-sql/src/app/(auth)/setup/page.tsx
+++ shape-sql/src/app/(auth)/setup/page.tsx
@@ -1,4 +1,9 @@
+'use client';
+import { useUser } from '@clerk/nextjs';
+
 export default function Page({ params }: { params: { id: string } }) {
+  const user = useUser();
+
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <h1>Dashboard</h1>
