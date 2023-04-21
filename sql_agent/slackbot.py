2db233b8c7e25dc0818d1e4dba28d121baac48f5
Meet Shah <meetcshah19@gmail.com>
2023-04-21 08:05:30 +0530

    add shape to dms and mentions

--- sql_agent/slackbot.py
+++ sql_agent/slackbot.py
@@ -5,11 +5,17 @@ from decouple import config
 from main import slackSqlChain
 
 app = App(token=config("SLACK_BOT_TOKEN"))
-final_answer_label = "Final Answer:"
 
-@app.message()
-def message_hello(message, say):
-    slackSqlChain(message['text'], message['user'], say, message['ts'])
+@app.event("app_mention")
+def message_hello(event, say):
+    user = app.client.users_profile_get(user=event['user'])
+    slackSqlChain(event['text'], user['profile']['real_name'], say, event['ts'])
+
+@app.event("message")
+def message_hello(event, say):
+    if(event['channel_type']== "im"):
+        user = app.client.users_profile_get(user=event['user'])
+        slackSqlChain(event['text'], user['profile']['real_name'], say, event['ts'])
 
 if __name__ == "__main__":
     SocketModeHandler(app, config("SLACK_APP_TOKEN")).start()
