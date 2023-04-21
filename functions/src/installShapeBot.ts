4d63747001c1eb0cce374d955db66b515c8f0302
Meet Shah <meetcshah19@gmail.com>
2023-04-21 05:42:16 +0530

    add oauth cloud function

---
+++ functions/src/installShapeBot.ts
@@ -0,0 +1,51 @@
+import * as functions from 'firebase-functions';
+import axios from 'axios';
+import FormData from 'form-data';
+
+export async function installShapeBotImpl(
+  request: functions.https.Request,
+  response: functions.Response
+) {
+  const code = request.query.code;
+  const userId = request.query.state;
+  let data = new FormData();
+  data.append('code', code);
+  data.append('client_id', '5104325368790.5123570453313');
+  data.append('client_secret', 'd38334f7efa4e1c968dbd2ed1b92f8a0');
+
+  let config = {
+    method: 'post',
+    url: 'https://slack.com/api/oauth.v2.access',
+    headers: {
+      ...data.getHeaders(),
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then((res) => {
+      console.log('access token: ' + res.data.access_token);
+      console.log('team id: ' + res.data.team.id);
+      axios.post('https://api.clerk.com/v1/organizations', {
+        name: res.data.team.id,
+        created_by: userId,
+        private_metadata: { botToken: res.data.access_token },
+      }, {
+        headers: {
+          'Authorization': `Bearer sk_test_MCqEnnuR8Xt4uYXZhFqknfxX0TnFdH4symiqGOaUtY`,
+        }
+      }).then((res) => {
+        response.status(200).send(); // redirect back to dashboard url here
+      }).catch((error) => {
+        response.status(500).send();
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export const installShapeBot = functions.https.onRequest(async (request, response) => {
+  installShapeBotImpl(request, response);
+});
--- sql_agent/slackbot.py
+++ sql_agent/slackbot.py
@@ -2,7 +2,6 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from decouple import config
-import json
 from main import slackSqlChain
 
 app = App(token=config("SLACK_BOT_TOKEN"))
@@ -11,7 +10,6 @@ final_answer_label = "Final Answer:"
 @app.message()
 def message_hello(message, say):
     slackSqlChain(message['text'], message['user'], say, message['ts'])
-    say("test")
 
 if __name__ == "__main__":
     SocketModeHandler(app, config("SLACK_APP_TOKEN")).start()
