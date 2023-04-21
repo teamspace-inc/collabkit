a1d36da02c2bb1da4f01f209df3ea52b6e7551f9
Namit Chadha <namit.chadha@gmail.com>
2023-04-21 10:37:13 +0100

    styles

--- shape-sql/src/app/(auth)/layout.tsx
+++ shape-sql/src/app/(auth)/layout.tsx
@@ -6,7 +6,20 @@ import { Logo } from '@/app/Logo';
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <nav className={styles.nav}>
+      <nav
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          height: '80px',
+          display: 'grid',
+          padding: '0 20px',
+          alignItems: 'center',
+          margin: '0 auto',
+          gridTemplateColumns: '6fr 180px 30px',
+        }}
+      >
         <Logo size="M" />
         <OrganizationSwitcher />
         <UserButton />
