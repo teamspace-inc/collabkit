a26d9c239a3ee8e70410601dfdf2cbb8453c8898
OmarTeamspace <128375683+OmarTeamspace@users.noreply.github.com>
2023-04-26 16:10:05 +0100

    Shape bot returns acknowledgement it recieved users query (#362)

--- sql_agent/shape_bot_app.py
+++ sql_agent/shape_bot_app.py
@@ -10,13 +10,19 @@ app = App(token=config("SLACK_BOT_TOKEN"))
 @app.event("app_mention")
 def message_hello(event, say):
     user = app.client.users_profile_get(user=event['user'])
-    slackSqlChain(event['text'], user['profile']['real_name'], say, event['ts'])
+    slackSqlChain(query=event['text'],
+                  username=user['profile']['real_name'],
+                  sendMessage=say,
+                  threadTs=event['ts'])
 
 @app.event("message")
 def message_hello(event, say):
     if(event['channel_type']== "im"):
         user = app.client.users_profile_get(user=event['user'])
-        slackSqlChain(event['text'], user['profile']['real_name'], say, event['ts'])
+        slackSqlChain(query=event['text'],
+                      username=user['profile']['real_name'], 
+                      sendMessage=say, 
+                      threadTs=event['ts'])
 
 if __name__ == "__main__":
     SocketModeHandler(app, config("SLACK_APP_TOKEN")).start()
