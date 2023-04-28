c94e67eb0c56e038e16624e7bb45fd1b97a730ab
OmarTeamspace <128375683+OmarTeamspace@users.noreply.github.com>
2023-04-28 14:46:45 +0100

    Add Snowflake integration, database factory, update copy, python debug configuration file (#376)
    Add Snowflake integration, database factory, update copy

---
+++ sql_agent/database_factory.py
@@ -0,0 +1,41 @@
+from enum import Enum
+from langchain.sql_database import SQLDatabase
+from sqlalchemy.engine import create_engine
+import json
+from decouple import config
+from snowflake.sqlalchemy import URL
+import os
+
+class DatabaseType(Enum):
+    BigQuery = "big_query"
+    Snowflake = "snowflake"
+
+class DatabaseFactory:
+    @classmethod
+    def create_database(cls) ->SQLDatabase:
+        db_type = DatabaseType(config("DB_TYPE"))
+
+        if DatabaseType.BigQuery == db_type:
+            return cls.__create_bigquery_database()
+        elif DatabaseType.Snowflake == db_type:
+            return cls.__create_snowflake_database()
+        else:
+            raise Exception("Database Type not supported")
+        
+    @classmethod
+    def __create_bigquery_database(cls) -> SQLDatabase:
+        engine = create_engine("bigquery://", credentials_info=json.loads(config("BQ_API_KEY")))
+        return SQLDatabase(engine=engine)
+    
+    @classmethod
+    def __create_snowflake_database(cls) -> SQLDatabase:
+        engine = create_engine(URL(
+          account = config("SNOWFLAKE_ACCOUNT"),
+          user = config("SNOWFLAKE_USER"),
+          password  = config("SNOWFLAKE_PASSWORD"),
+          database = config("SNOWFLAKE_DATABASE"),
+          schema = config("SNOWFLAKE_SCHEMA"),
+        ))
+        connection = engine.connect()
+        return SQLDatabase(engine=engine, 
+                           include_tables=[config("SNOWFLAKE_TABLE")]) #TODO: Create Vector Store & Embeddings
