abe6a627d59961f69e96658d56479467f25eef50
OmarTeamspace <128375683+OmarTeamspace@users.noreply.github.com>
2023-05-05 14:50:06 +0100

    Omar/analytics defaults (#387)
    * Disable analytics if api key not set
    
    * rename file
    
    * Rename imports
    
    * rename key

---
+++ sql_agent/shape_analytics.py
@@ -0,0 +1,15 @@
+from mixpanel import Mixpanel
+from decouple import config
+
+class ShapeAnalytics:
+
+  def __init__(self, distinctId: str):
+    shape_analytics_api_key = config("SHAPE_ANALYTICS_API_KEY", default=None)
+    # If customer has not opted into analytics via shape analytics api key, don't initialize Mixpanel
+    self.mixpanel = None if shape_analytics_api_key is None else Mixpanel(shape_analytics_api_key)
+    self.distinctId = distinctId
+
+  def track(self, event_name: str, properties=None, meta=None) -> None:
+    if self.mixpanel is None:
+      return
+    self.mixpanel.track(self.distinctId, event_name, properties, meta)
--- sql_agent/ss_analytics.py
+++
@@ -1,9 +0,0 @@
-from mixpanel import Mixpanel
-
-class ShapeAnalytics:
-  def __init__(self, mixpanel: Mixpanel, distinctId: str):
-    self.mixpanel = mixpanel
-    self.distinctId = distinctId
-    
-  def track(self, event_name: str, properties=None, meta=None):
-    self.mixpanel.track(self.distinctId, event_name, properties, meta)
